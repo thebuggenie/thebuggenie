f1128d237232cdd92187301b84c1b038eb39c6bf
zegenie <zegenie@gmail.com>
2009-10-01 09:17:59 +0000

    now nailed project menu strip look, added tab effect for (currently unavailable) sections

--- modules/main/templates/_project_menustrip.inc.php
+++ modules/main/templates/_project_menustrip.inc.php
@@ -1,14 +1,16 @@
-<div class="menu_project_strip">
+<div class="menu_project_strip tab_menu">
 	<?php if ($project instanceof BUGSproject): ?>
 		<div class="project_name">
 			<?php echo image_tag('spinning_32.gif', array('id' => 'project_menustrip_indicator', 'style' => 'display: none;')); ?>
 			<span id="project_menustrip_name"><?php echo $project->getName(); ?></span>
 		</div>
 		<div class="project_stuff">
-			<a href="#" class="faded_medium">Project dashboard</a> | 
-			<a href="#" class="faded_medium">Planning</a> | 
-			<a href="#" class="faded_medium">Issues</a> | 
-			<a href="#" class="faded_medium">Team</a>
+		<ul>
+			<li><a href="#" class="faded_medium">Project dashboard</a></li>
+			<li><a href="#" class="faded_medium">Planning</a></li>
+			<li><a href="#" class="faded_medium">Issues</a></li>
+			<li><a href="#" class="faded_medium">Team</a></li>
+		</ul> 
 		</div>
 		<?php if ($bugs_response->getPage() != 'reportissue' && (!isset($hide_button) || (isset($hide_button) && $hide_button == false))): ?>
 			<form action="<?php echo make_url('project_reportissue', array('project_key' => $project->getKey())); ?>" method="get" style="clear: none; display: inline; position: absolute; right: 3px; top: 3px;">
