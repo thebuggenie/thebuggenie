8214ca2c2bb53194f082315dbd4600248a2931f8
zegenie <zegenie@gmail.com>
2009-11-19 15:48:20 +0000

    created some wiki buttons (ok, only one, and it does nothing)

--- modules/publish/templates/showarticle.php
+++ modules/publish/templates/showarticle.php
@@ -14,15 +14,15 @@
 					<?php echo get_spaced_name($article_name); ?>
 				</div>
 				<div style="font-size: 14px;">
-					<?php echo __('This article has not been created yet. Click below to create it and start editing.'); ?>
-					<div class="publish_article_actions">
-						<div class="sub_header"><?php echo __('Actions available'); ?></div>
-						<form action="<?php echo make_url('publish_article_edit', array('article_name' => $article_name)); ?>" method="get">
-							<input type="submit" value="Edit this article">
-						</form>
-					</div>
+					<?php echo __('This is a placeholder for an article that has not been created yet. You can create it by clicking %create_this_article% below.', array('%create_this_article%' => '<b>'.__('Create this article').'</b>')); ?>
 				</div>
 			<?php endif; ?>
+			<div class="publish_article_actions">
+				<div class="sub_header"><?php echo __('Actions available'); ?></div>
+				<form action="<?php echo make_url('publish_article_edit', array('article_name' => $article_name)); ?>" method="get">
+					<input type="submit" value="<?php echo ($article instanceof PublishArticle) ? __('Edit this article') : __('Create this article'); ?>">
+				</form>
+			</div>
 		</td>
 	</tr>
 </table>
