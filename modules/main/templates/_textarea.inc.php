2b03509884f8b4fd7df269a142b8010a5ac77a3a
zegenie <zegenie@gmail.com>
2009-11-26 13:12:17 +0000

    update formatting tips and textarea

--- modules/main/templates/_textarea.inc.php
+++ modules/main/templates/_textarea.inc.php
@@ -2,6 +2,6 @@
 <?php if (!isset($hide_hint) || $hide_hint == false): ?>
 	<div class="textarea_hint">
 		<div class="header"><?php echo __('Formatting tips'); ?></div>
-		<p><?php echo __('URLs will be automatically transformed to links - to specify a custom link title, write it in this format: %url_example%.<br>To link to an existing issue, write "issue" or "ticket" followed by the issue number (ex: "%ticket_example%").<br>If you want to paste a block of code or a quote, enclose it in [code][/code] or [quote][/quote] tags.', array('%url_example%' => '<span class="example">[url=http://www.somewhere.com]Link title[/url]</span>', '%ticket_example%' => '<span class="example">ticket #3</span>')); ?></p>
+		<p><?php echo __('URLs will be automatically transformed to links. To link to an existing issue, write "issue", "bug" or "ticket" followed by the issue number (ex: "%ticket_example_1%" or "%ticket_example_2%").<br>Enclose code samples and comments in <code></code> and <quote></quote> tags. See more formatting tips in %wiki_formatting%.', array('%ticket_example_1%' => '<span class="example">ticket #3</span>', '%ticket_example_2%' => '<span class="example">ticket MYPROJ-1</span>', '%wiki_formatting%' => link_tag(make_url('publish_article', array('article_name' => 'WikiFormatting')), 'WikiFormatting'))); ?></p>
 	</div>
 <?php endif; ?>
