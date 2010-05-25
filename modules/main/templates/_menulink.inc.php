94887c507582c8e222a7c4cccc99f8b0e4441f4b
zegenie <zegenie@gmail.com>
2010-05-25 14:06:58 +0000

    some more menu changes

--- modules/main/templates/_menulink.inc.php
+++ modules/main/templates/_menulink.inc.php
@@ -26,19 +26,15 @@
 <?php if ($tbg_user->canEditMainMenu()): ?>
 	<tr id="<?php echo $link['target_type']; ?>_<?php echo $link['target_id']; ?>_links_<?php echo $link_id; ?>_remove_confirm" style="display: none;">
 		<td colspan="2">
-			<div class="rounded_box white" style="position: relative; clear: both; left: auto; top: auto; margin: 0; width: auto;">
-				<b class="xtop"><b class="xb1"></b><b class="xb2"></b><b class="xb3"></b><b class="xb4"></b></b>
-				<div class="xboxcontent" style="padding: 0 5px 5px 5px; font-size: 12px; width: auto;">
-					<div class="header_div" style="margin-top: 0;"><?php echo __('Are you sure?'); ?></div>
-					<div class="content" style="padding: 3px;">
-						<?php echo __('Do you really want to remove this item from the menu?'); ?>
-						<div style="text-align: right;">
-							<?php echo javascript_link_tag(__('Yes'), array('onclick' => "$('{$link['target_type']}_{$link['target_id']}_links_{$link_id}_remove_confirm').toggle();removeLink('".make_url('remove_link', array('target_type' => $link['target_type'], 'target_id' => $link['target_id'], 'link_id' => $link_id))."', '{$link['target_type']}', '{$link['target_id']}', ".$link_id.");")); ?> ::
-							<?php echo javascript_link_tag('<b>'.__('No').'</b>', array('onclick' => "$('{$link['target_type']}_{$link['target_id']}_links_{$link_id}_remove_confirm').toggle();")); ?>
-						</div>
+			<div class="rounded_box white shadowed" style="position: absolute; padding: 0 5px 5px 5px; font-size: 12px; width: 300px; z-index: 10001;">
+				<div class="header_div" style="margin-top: 0;"><?php echo __('Are you sure?'); ?></div>
+				<div class="content" style="padding: 3px;">
+					<?php echo __('Do you really want to remove this item from the menu?'); ?>
+					<div style="text-align: right;">
+						<?php echo javascript_link_tag(__('Yes'), array('onclick' => "$('{$link['target_type']}_{$link['target_id']}_links_{$link_id}_remove_confirm').toggle();removeLink('".make_url('remove_link', array('target_type' => $link['target_type'], 'target_id' => $link['target_id'], 'link_id' => $link_id))."', '{$link['target_type']}', '{$link['target_id']}', ".$link_id.");")); ?> ::
+						<?php echo javascript_link_tag('<b>'.__('No').'</b>', array('onclick' => "$('{$link['target_type']}_{$link['target_id']}_links_{$link_id}_remove_confirm').toggle();")); ?>
 					</div>
 				</div>
-				<b class="xbottom"><b class="xb4"></b><b class="xb3"></b><b class="xb2"></b><b class="xb1"></b></b>
 			</div>
 		</td>
 	</tr>
--- modules/main/templates/_menulinks.inc.php
+++ modules/main/templates/_menulinks.inc.php
@@ -6,6 +6,30 @@
 		<?php endif; ?>
 		<?php echo $title; ?>
 	</div>
+	<?php if ($tbg_user->canEditMainMenu()): ?>
+		<div class="rounded_box lightgrey shadowed" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>" style="position: absolute; width: 300px; z-index: 10001; margin: 5px 0 5px 5px; display: none">
+			<div class="header_div" style="margin: 0 0 5px 0;"><?php echo __('Add a link'); ?>:</div>
+			<form action="<?php echo make_url('attach_link', array('target_type' => $target_type, 'target_id' => $target_id)); ?>" method="post" onsubmit="addLink('<?php echo make_url('attach_link', array('target_type' => $target_type, 'target_id' => $target_id)); ?>', '<?php echo $target_type; ?>', '<?php echo $target_id; ?>');return false;" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_form">
+				<dl style="margin: 0;">
+					<dt style="width: 80px; padding-top: 3px;"><label for="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_url"><?php echo ($target_type == 'wiki') ? __('Article name') : __('URL'); ?>:</label></dt>
+					<dd style="margin-bottom: 0px;"><input type="text" name="link_url" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_url" style="width: 95%;"></dd>
+					<dt style="width: 80px; font-size: 10px; padding-top: 4px;"><label for="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_description"><?php echo __('Description'); ?>:</label></dt>
+					<dd style="margin-bottom: 0px;"><input type="text" name="description" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_description" style="width: 95%;"></dd>
+				</dl>
+				<div style="font-size: 12px; padding: 15px 2px 10px 2px;" class="faded_medium" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_submit">
+					<?php if ($target_type == 'wiki'): ?>
+						<?php echo __('Enter the name of the article to link to here, along with an (optional) description, and press "%add_link%" to add it to the menu.', array('%add_link%' => __('Add link'))); ?><br /><br />
+					<?php else: ?>
+						<?php echo __('Enter the link URL here, along with an (optional) description, and press "%add_link%" to add it to the menu.', array('%add_link%' => __('Add link'))); ?><br /><br />
+					<?php endif; ?>
+					<?php echo __('To add free text, just enter text in the description - without any url - and press the "%add_link%" button (Text will be parsed according to the %wiki_formatting%).', array('%add_link%' => __('Add link'), '%wiki_formatting%' => link_tag(make_url('publish_article', array('article_name' => 'WikiFormatting')), 'WikiFormatting'))); ?><br /><br />
+					<?php echo __('To add a spacer, just press "%add_link%", without any url or description.', array('%add_link%' => __('Add link'))); ?>
+					<div style="text-align: center; padding: 10px; display: none;" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_indicator"><?php echo image_tag('spinning_26.gif'); ?></div>
+					<div style="text-align: center;"><input type="submit" value="<?php echo __('Add link'); ?>" style="font-weight: bold;"><?php echo __('%attach_link% or %cancel%', array('%attach_link%' => '', '%cancel%' => '<b>'.javascript_link_tag(__('cancel'), array('onclick' => "$('attach_link_{$target_type}_{$target_id}').toggle();")).'</b>')); ?></div>
+				</div>
+			</form>
+		</div>
+	<?php endif; ?>
 	<div class="content">
 		<table style="table-layout: fixed; width: 100%;" cellpadding=0 cellspacing=0>
 			<tbody id="<?php echo $target_type; ?>_<?php echo $target_id; ?>_links">
@@ -17,27 +41,3 @@
 		<div style="padding-left: 5px;<?php if (count($links) > 0): ?> display: none;<?php endif; ?>" class="no_items" id="<?php echo $target_type; ?>_<?php echo $target_id; ?>_no_links"><?php echo __('There are no links in this menu'); ?></div>
 	</div>
 </div>
-<?php if ($tbg_user->canEditMainMenu()): ?>
-	<div class="rounded_box borderless lightgrey" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>" style="margin: 5px 0 5px 5px; display: none">
-		<div class="header_div" style="margin: 0 0 5px 0;"><?php echo __('Add a link'); ?>:</div>
-		<form action="<?php echo make_url('attach_link', array('target_type' => $target_type, 'target_id' => $target_id)); ?>" method="post" onsubmit="addLink('<?php echo make_url('attach_link', array('target_type' => $target_type, 'target_id' => $target_id)); ?>', '<?php echo $target_type; ?>', '<?php echo $target_id; ?>');return false;" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_form">
-			<dl style="margin: 0;">
-				<dt style="width: 80px; padding-top: 3px;"><label for="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_url"><?php echo ($target_type == 'wiki') ? __('Article name') : __('URL'); ?>:</label></dt>
-				<dd style="margin-bottom: 0px;"><input type="text" name="link_url" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_url" style="width: 95%;"></dd>
-				<dt style="width: 80px; font-size: 10px; padding-top: 4px;"><label for="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_description"><?php echo __('Description'); ?>:</label></dt>
-				<dd style="margin-bottom: 0px;"><input type="text" name="description" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_description" style="width: 95%;"></dd>
-			</dl>
-			<div style="font-size: 12px; padding: 15px 2px 10px 2px;" class="faded_medium" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_submit">
-				<?php if ($target_type == 'wiki'): ?>
-					<?php echo __('Enter the name of the article to link to here, along with an (optional) description, and press "%add_link%" to add it to the menu.', array('%add_link%' => __('Add link'))); ?><br /><br />
-				<?php else: ?>
-					<?php echo __('Enter the link URL here, along with an (optional) description, and press "%add_link%" to add it to the menu.', array('%add_link%' => __('Add link'))); ?><br /><br />
-				<?php endif; ?>
-				<?php echo __('To add free text, just enter text in the description - without any url - and press the "%add_link%" button (Text will be parsed according to the %wiki_formatting%).', array('%add_link%' => __('Add link'), '%wiki_formatting%' => link_tag(make_url('publish_article', array('article_name' => 'WikiFormatting')), 'WikiFormatting'))); ?><br /><br />
-				<?php echo __('To add a spacer, just press "%add_link%", without any url or description.', array('%add_link%' => __('Add link'))); ?>
-				<div style="text-align: center; padding: 10px; display: none;" id="attach_link_<?php echo $target_type; ?>_<?php echo $target_id; ?>_indicator"><?php echo image_tag('spinning_26.gif'); ?></div>
-				<div style="text-align: center;"><input type="submit" value="<?php echo __('Add link'); ?>" style="font-weight: bold;"><?php echo __('%attach_link% or %cancel%', array('%attach_link%' => '', '%cancel%' => '<b>'.javascript_link_tag(__('cancel'), array('onclick' => "$('attach_link').toggle();")).'</b>')); ?></div>
-			</div>
-		</form>
-	</div>
-<?php endif; ?>
