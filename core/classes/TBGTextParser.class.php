231195c2a2920e50a843e79c555f5448da858a1a
lsproc <kentphilip@gmail.com>
2010-07-31 08:54:59 +0000

    - Well and truly and completely and utterly fix syntax highlighting - Improve the readability for the documentation for the above

--- core/classes/TBGTextParser.class.php
+++ core/classes/TBGTextParser.class.php
@@ -722,7 +722,7 @@
 			
 			$text = preg_replace_callback('/<nowiki>(.+?)<\/nowiki>(?!<\/nowiki>)/ism', array($this, "_parse_save_nowiki"), $text);
 			//$text = preg_replace_callback('/<source((?:\s+[^\s]+=".*?")*)>\s*(.*?)\s*<\/source>/ism', array($this, "_parse_save_code"), $text);
-			$text = preg_replace_callback('/<source((?:\s+[^\s]+=".*?")*)>\s*(.*+)\s*<\/source>/ism', array($this, "_parse_save_code"), $text);
+			$text = preg_replace_callback('/<source((?:\s+[^\s]+=".*?")*)>\s*(.+)\s*<\/source>/ism', array($this, "_parse_save_code"), $text);
 			// Thanks to Mike Smith (scgtrp) for the above regexp
 
 			$lines = explode("\n", $text);
