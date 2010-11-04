b3d7f5840a64cf1435ad190d64412ecdd5f2aa45
edno <g.heitz@yahoo.fr>
2010-11-04 13:22:58 +0000

    Fix "button vs. spin pic" caching error when going back to the previous installation step (bug #657 & #570)

--- modules/installation/templates/_header.inc.php
+++ modules/installation/templates/_header.inc.php
@@ -2,7 +2,7 @@
 
     header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
-    header ("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+    header ("Cache-Control: no-store, must-revalidate"); // HTTP/1.1
     header ("Pragma: no-cache"); // HTTP/1.0
 
     $step = $tbg_request->getParameter('step', 0);
