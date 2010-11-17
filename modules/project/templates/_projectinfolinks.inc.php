3a0e65a71b53810f908fcf7e7d238ec9c17b5c3f
zegenie <zegenie@gmail.com>
2010-11-17 13:11:04 +0000

    add planning page to project info

--- modules/project/templates/_projectinfolinks.inc.php
+++ modules/project/templates/_projectinfolinks.inc.php
@@ -12,6 +12,7 @@
 <?php if (TBGContext::getCurrentProject()->usesScrum()): ?>
 	<?php echo link_tag(make_url('project_scrum', array('project_key' => TBGContext::getCurrentProject()->getKey())), __('Sprint planning'), (($tbg_response->getPage() == 'project_scrum') ? array('class' => 'selected') : array())); ?>
 <?php endif; ?>
+<?php echo link_tag(make_url('project_planning', array('project_key' => TBGContext::getCurrentProject()->getKey())), __('Planning'), (($tbg_response->getPage() == 'project_planning') ? array('class' => 'selected') : array())); ?>
 <?php echo link_tag(make_url('project_roadmap', array('project_key' => TBGContext::getCurrentProject()->getKey())), __('Roadmap'), (($tbg_response->getPage() == 'project_roadmap') ? array('class' => 'selected') : array())); ?>
 <?php echo link_tag(make_url('project_team', array('project_key' => TBGContext::getCurrentProject()->getKey())), __('Team overview'), (($tbg_response->getPage() == 'project_team') ? array('class' => 'selected') : array())); ?>
 <?php echo link_tag(make_url('project_statistics', array('project_key' => TBGContext::getCurrentProject()->getKey())), __('Statistics'), (($tbg_response->getPage() == 'project_statistics') ? array('class' => 'selected') : array())); ?>
--- modules/project/templates/planning.html.php
+++ modules/project/templates/planning.html.php
@@ -5,7 +5,12 @@
 	$tbg_response->setTitle(__('"%project_name%" project planning', array('%project_name%' => $selected_project->getName())));
 
 ?>
-<div style="width: 330px; padding: 5px; margin: 5px 3px 5px 3px;">
-	<?php foreach ($recent_ideas as $idea): ?>
-	<?php endforeach; ?>
-</div>
+			<?php include_template('project/projectinfosidebar', array('selected_project' => $selected_project)); ?>
+			<div style="padding: 15px; color: #AAA; font-size: 12px;"><?php echo __('Interactive planning has not been added yet'); ?></div>
+			<div style="width: 330px; padding: 5px; margin: 5px 3px 5px 3px;">
+				<?php /*foreach ($recent_ideas as $idea): ?>
+				<?php endforeach; */ ?>
+			</div>
+		</td>
+	</tr>
+</table>
