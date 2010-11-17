d4c1c84d461c3e16291fa1448880fd650fd6e2c3
zegenie <zegenie@gmail.com>
2010-11-17 12:24:18 +0000

    make "issues" tab dropdown saved searches (only predefined searches for now)

--- core/templates/header.inc.php
+++ core/templates/header.inc.php
@@ -90,7 +90,7 @@
 										<?php if (!TBGSettings::isSingleProjectTracker() && !TBGContext::isProjectContext()): ?>
 											<li<?php if ($tbg_response->getPage() == 'home'): ?> class="selected"<?php endif; ?>><?php echo link_tag(make_url('home'), image_tag('tab_index.png').__('Frontpage')); ?></li>
 										<?php elseif (TBGContext::isProjectContext()): ?>
-											<li<?php if (in_array($tbg_response->getPage(), array('project_dashboard', 'project_timeline', 'project_team', 'project_roadmap', 'project_statistics'))): ?> class="selected"<?php endif; ?>>
+											<li<?php if (in_array($tbg_response->getPage(), array('project_dashboard', 'project_scrum', 'project_timeline', 'project_team', 'project_roadmap', 'project_statistics'))): ?> class="selected"<?php endif; ?>>
 												<div>
 													<?php echo link_tag(make_url('project_dashboard', array('project_key' => TBGContext::getCurrentProject()->getKey())), image_tag('icon_dashboard_small.png').__('Project information')); ?>
 													<?php echo javascript_link_tag(image_tag('tabmenu_dropdown.png', array('class' => 'menu_dropdown')), array('onmouseover' => "")); ?>
@@ -120,7 +120,27 @@
 											</li>
 										<?php endif; ?>
 										<?php if (TBGContext::isProjectContext() && $tbg_user->canSearchForIssues()): ?>
-											<li<?php if (in_array($tbg_response->getPage(), array('project_issues', 'viewissue'))): ?> class="selected"<?php endif; ?>><?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey())), image_tag('tab_search.png').__('Issues')); ?></li>
+											<li<?php if (in_array($tbg_response->getPage(), array('project_issues', 'viewissue'))): ?> class="selected"<?php endif; ?>>
+												<div>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey())), image_tag('tab_search.png').__('Issues')); ?>
+													<?php echo javascript_link_tag(image_tag('tabmenu_dropdown.png', array('class' => 'menu_dropdown')), array('onmouseover' => "")); ?>
+												</div>
+												<div id="issues_menu" class="tab_menu_dropdown shadowed">
+												<?php if (TBGContext::isProjectContext()): ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_PROJECT_OPEN_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Open issues for this project')); ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_PROJECT_CLOSED_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Closed issues for this project')); ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_PROJECT_MILESTONE_TODO, 'search' => true)), image_tag('icon_savedsearch.png') . __('Milestone todo-list for this project')); ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_PROJECT_MOST_VOTED, 'search' => true)), image_tag('icon_savedsearch.png') . __('Most voted for issues')); ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_MY_REPORTED_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Issues reported by me')); ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_MY_ASSIGNED_OPEN_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Open issues assigned to me')); ?>
+													<?php echo link_tag(make_url('project_issues', array('project_key' => TBGContext::getCurrentProject()->getKey(), 'predefined_search' => TBGContext::PREDEFINED_SEARCH_TEAM_ASSIGNED_OPEN_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Open issues assigned to my teams')); ?>
+												<?php else: ?>
+													<?php echo link_tag(make_url('search', array('predefined_search' => TBGContext::PREDEFINED_SEARCH_MY_REPORTED_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Issues reported by me')); ?>
+													<?php echo link_tag(make_url('search', array('predefined_search' => TBGContext::PREDEFINED_SEARCH_MY_ASSIGNED_OPEN_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Open issues assigned to me')); ?>
+													<?php echo link_tag(make_url('search', array('predefined_search' => TBGContext::PREDEFINED_SEARCH_TEAM_ASSIGNED_OPEN_ISSUES, 'search' => true)), image_tag('icon_savedsearch.png') . __('Open issues assigned to my teams')); ?>
+												<?php endif; ?>
+												</div>
+											</li>
 										<?php endif; ?>
 										<?php TBGEvent::createNew('core', 'menustrip_item_links', null, array('selected_tab' => $tbg_response->getPage()))->trigger(); ?>
 										<?php if (!TBGContext::isProjectContext() && $tbg_user->canAccessConfigurationPage()): ?>
