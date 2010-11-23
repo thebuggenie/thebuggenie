29c639e354c45ba2cda160d5824fd94d3eed6faf
zegenie <zegenie@gmail.com>
2010-11-23 10:52:11 +0000

    don't show wiki dropdown when there are no options

--- modules/publish/templates/_menustriplinks.inc.php
+++ modules/publish/templates/_menustriplinks.inc.php
@@ -5,19 +5,21 @@
 			<?php echo javascript_link_tag(image_tag('tabmenu_dropdown.png', array('class' => 'menu_dropdown'))); ?>
 		<?php endif; ?>
 	</div>
-	<div id="wiki_dropdown_menu" class="tab_menu_dropdown shadowed">
-		<?php if (TBGContext::isProjectContext()): ?>
-			<div class="header"><?php echo __('Currently selected project'); ?></div>
-			<?php echo link_tag($project_url, __('Project wiki frontpage')); ?>
-		<?php endif; ?>
-		<div class="header"><?php echo __('Global content'); ?></div>
-		<?php echo link_tag($url, TBGContext::getModule('publish')->getMenuTitle(false)); ?>
-		<?php if (count(TBGProject::getAll()) > (int) TBGContext::isProjectContext()): ?>
-			<div class="header"><?php echo __('Project wikis'); ?></div>
-			<?php foreach (TBGProject::getAll() as $project): ?>
-				<?php if (isset($project_url) && $project->getID() == TBGContext::getCurrentProject()->getID()) continue; ?>
-				<?php echo link_tag(make_url('publish_article', array('article_name' => ucfirst($project->getKey()).':MainPage')), $project->getName()); ?>
-			<?php endforeach; ?>
-		<?php endif; ?>
-	</div>
+	<?php if (count(TBGProject::getAll())): ?>
+		<div id="wiki_dropdown_menu" class="tab_menu_dropdown shadowed">
+			<?php if (TBGContext::isProjectContext()): ?>
+				<div class="header"><?php echo __('Currently selected project'); ?></div>
+				<?php echo link_tag($project_url, __('Project wiki frontpage')); ?>
+			<?php endif; ?>
+			<div class="header"><?php echo __('Global content'); ?></div>
+			<?php echo link_tag($url, TBGContext::getModule('publish')->getMenuTitle(false)); ?>
+			<?php if (count(TBGProject::getAll()) > (int) TBGContext::isProjectContext()): ?>
+				<div class="header"><?php echo __('Project wikis'); ?></div>
+				<?php foreach (TBGProject::getAll() as $project): ?>
+					<?php if (isset($project_url) && $project->getID() == TBGContext::getCurrentProject()->getID()) continue; ?>
+					<?php echo link_tag(make_url('publish_article', array('article_name' => ucfirst($project->getKey()).':MainPage')), $project->getName()); ?>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</div>
+	<?php endif; ?>
 </li>
