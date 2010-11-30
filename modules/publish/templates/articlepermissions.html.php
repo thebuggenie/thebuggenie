211ae0e8bb20f9ad8355eab721b68e8c7f924067
zegenie <zegenie@gmail.com>
2010-11-30 12:38:59 +0000

    article permission description tweaking

--- modules/publish/templates/articlepermissions.html.php
+++ modules/publish/templates/articlepermissions.html.php
@@ -48,7 +48,15 @@
 								<?php echo __('Specify permissions for the %namespace% namespace', array('%namespace%' => '<span class="namespace">'.$namespace.'</span>')); ?>
 							<?php endif; ?>
 						</div>
-						<?php echo __('Select this option to specify permissions for the above namespace. These permissions will apply for all articles in the mentioned namespace for which article-specific permissions have not been granted.'); ?>
+						<?php if (is_numeric($namespace) && $namespace == 0): ?>
+							<?php echo __('Select this option to specify permissions for the entire wiki.'); ?>
+						<?php elseif ($namespace == $article->getName()): ?>
+							<?php echo __('Select this option to specify permissions for this article.'); ?>
+						<?php elseif ($namespace == "Category"): ?>
+							<?php echo __('Select this option to specify permissions for who can create and edit categories.'); ?>
+						<?php else: ?>
+							<?php echo __('Specify permissions for the %namespace% namespace. These permissions will apply for all articles in the mentioned namespace for which article-specific permissions, or child-namespace permissions have not been granted.', array('%namespace%' => '<i>'.$namespace.'</i>')); ?>
+						<?php endif; ?>
 						<div style="text-align: right; padding: 10px;">
 							<button onclick="$('publish_<?php echo $namespace; ?>_editarticle_permissions').toggle();"><?php echo __('Edit write permissions'); ?></button>
 							<button onclick="$('publish_<?php echo $namespace; ?>_deletearticle_permissions').toggle();"><?php echo __('Edit delete permissions'); ?></button>
