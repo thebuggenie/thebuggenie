1b88c3266483066127563a48cce9cb201111e4e4
lsproc <kentphilip@gmail.com>
2010-12-24 16:26:02 +0000

    Don't break the add affected items box if one of the affected item types are disabled

--- modules/main/templates/_issueadditem.inc.php
+++ modules/main/templates/_issueadditem.inc.php
@@ -10,27 +10,27 @@
 		<?php if ($issue->getProject()->isBuildsEnabled()): ?><input type="radio" name="item_type" id="item_type_build" value="build" onClick="$('no_type').hide(); $('which_item_edition').hide(); $('which_item_component').hide(); $('which_item_build').show(); $('item_submit').show();" /><label for="item_type_build"> <?php echo __('Release'); ?></label><br /><?php endif; ?>
 		<div class="header_div"><?php echo __('Affected item'); ?></div>
 		<div class="faded_out" id="no_type" style="padding-top: 10px;"><?php echo('Please select an item type'); ?></div>
-		<?php if ($issue->getProject()->isEditionsEnabled()): ?>
 		<select name="which_item_edition" id="which_item_edition" style="width: 100%; margin-top: 10px; display: none;">
+		<?php if ($issue->getProject()->isEditionsEnabled()): ?>
 			<?php foreach ($editions as $edition): ?>
 			<option value="<?php echo $edition->getID(); ?>"><?php echo $edition->getName(); ?></option>
 			<?php endforeach; ?>
-		</select>
 		<?php endif; ?>
-		<?php if ($issue->getProject()->isComponentsEnabled()): ?>
+		</select>
 		<select name="which_item_component" id="which_item_component" style="width: 100%; margin-top: 10px; display: none;">
+		<?php if ($issue->getProject()->isComponentsEnabled()): ?>
 			<?php foreach ($components as $component): ?>
 			<option value="<?php echo $component->getID(); ?>"><?php echo $component->getName(); ?></option>
 			<?php endforeach; ?>
-		</select>
 		<?php endif; ?>
-		<?php if ($issue->getProject()->isBuildsEnabled()): ?>
+		</select>
 		<select name="which_item_build" id="which_item_build" style="width: 100%; margin-top: 10px; display: none;">
+		<?php if ($issue->getProject()->isBuildsEnabled()): ?>
 			<?php foreach ($builds as $build): ?>
 			<option value="<?php echo $build->getID(); ?>"><?php echo $build->getName(); ?></option>
 			<?php endforeach; ?>
-		</select>
 		<?php endif; ?>
+		</select>
 		<input type="submit" style="display: none; margin-top: 10px;" id="item_submit" value="<?php echo __('Add this item'); ?>">
 		<?php echo image_tag('spinning_20.gif', array('id' => 'add_affected_spinning', 'style' => 'display: none;')); ?>
 		</form>
