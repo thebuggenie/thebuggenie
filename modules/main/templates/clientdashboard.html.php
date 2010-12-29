fa88ce6727d6ae6d43e765ec814080701220dc29
edno <g.heitz@yahoo.fr>
2010-12-29 10:42:02 +0000

    minor change of client dashoard display (client details)

--- modules/main/templates/clientdashboard.html.php
+++ modules/main/templates/clientdashboard.html.php
@@ -18,9 +18,23 @@
 <?php if ($client instanceof TBGClient): ?>
 	<div class="client_dashboard">
 		<div class="dashboard_client_info">
-			<span class="dashboard_client_header"><?php echo $client->getName(); ?></span><br />
-			<b><?php echo __('Website:'); ?></b> <?php if ($client->getWebsite() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><a href="<?php echo $client->getWebsite(); ?>" target="_blank"><?php echo $client->getWebsite(); ?></a><?php endif; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><?php echo __('Email address:'); ?></b> <?php if ($client->getEmail() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><a href="mailto:<?php echo $client->getEmail(); ?>" target="_blank"><?php echo $client->getEmail(); ?></a><?php endif; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><?php echo __('Telephone:'); ?></b> <?php if ($client->getTelephone() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><?php echo $client->getTelephone(); endif; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><?php echo __('Fax:'); ?></b> <?php if ($client->getFax() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><?php echo $client->getFax(); endif; ?>
-			<br />
+			<span class="dashboard_client_header"><?php echo $client->getName(); ?></span>
+			<table>
+				<tr>
+					<td style="padding-right: 10px">
+						<b><?php echo __('Website:'); ?></b> <?php if ($client->getWebsite() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><a href="<?php echo $client->getWebsite(); ?>" target="_blank"><?php echo $client->getWebsite(); ?></a><?php endif; ?>
+					</td>
+					<td style="padding: 0px 10px">
+						<b><?php echo __('Email address:'); ?></b> <?php if ($client->getEmail() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><a href="mailto:<?php echo $client->getEmail(); ?>" target="_blank"><?php echo $client->getEmail(); ?></a><?php endif; ?>
+					</td>
+					<td style="padding: 0px 10px">
+						<b><?php echo __('Telephone:'); ?></b> <?php if ($client->getTelephone() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><?php echo $client->getTelephone(); endif; ?>
+					</td>
+					<td style="padding: 0px 10px">
+						<b><?php echo __('Fax:'); ?></b> <?php if ($client->getFax() == ''): ?><span class="faded_out"><?php echo __('none'); ?></span><?php else: ?><?php echo $client->getFax(); endif; ?>
+					</td>
+				</tr>
+			</table>
 		</div>
 
 		<table class="client_dashboard_table">
