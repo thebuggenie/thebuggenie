e2eaca9d380102d0f9a7b9c962b305db88d93d2f
zegenie <zegenie@gmail.com>
2011-01-04 07:22:04 +0000

    fix broken custom fields in issue view

--- modules/main/classes/actioncomponents.class.php
+++ modules/main/classes/actioncomponents.class.php
@@ -104,7 +104,7 @@
 				$merged_methodname = "isCustomfield{$key}Merged";
 				$customfields_list[$key] = array('type' => $customdatatype->getType(),
 												'title' => $i18n->__($customdatatype->getDescription()),
-												'visible' => $this->issue->getIssuetype()->isFieldVisible($customdatatype->getKey()),
+												'visible' => $this->issue->isFieldVisible($customdatatype->getKey()),
 												'changed' => $this->issue->$changed_methodname(),
 												'merged' => $this->issue->$merged_methodname(),
 												'change_tip' => $i18n->__($customdatatype->getInstructions()),
