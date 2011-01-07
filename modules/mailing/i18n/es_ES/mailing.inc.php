f56c674db87a70c25a1b3eb437e915525f0c9f55
sareche <sareche@hotmail.com>
2011-01-07 12:52:59 +0000

    Cleaned and syncronized spanish translation with en_US

--- modules/mailing/i18n/es_ES/mailing.inc.php
+++ modules/mailing/i18n/es_ES/mailing.inc.php
@@ -1,29 +1,62 @@
 <?php
 
-	$strings['Yes'] = 'Sí';
-	$strings['No'] = 'No';
-	$strings['Save'] = 'Guardar';
+	$strings['Configure mail notification module'] = 'Configuración módulo de notificación por correo';
+	$strings['Set up the mail notification module here.'] = 'Defina las opciones del módulo de notificación por correo aquí.';
+	$strings['Set up email- and user notifications from this section.'] = 'Establezca las opciones de correo y notificación a usuarios desde esta sección.';
+	$strings['SMTP server address'] = 'Dirección del servidor SMTP';
+	$strings['The address BUGS 2 should use to send emails via'] = 'The address BUGS 2 should use to send emails via';
+	$strings['SMTP address port'] = 'Puerto del servidor SMTP';
+	$strings['The port used by the SMTP server (default is 25)'] = 'El puerto utilizado por el servidor SMTP (por defecto es 25)';
+	$strings['Server type'] = 'Tipo de servidor';
+	$strings['Select whether the SMTP server is a MS Exchange server'] = 'Seleccione si el servidor de SMTP es un servidor MS Exchange';
+	$strings['SMTP username'] = 'Nombre de usuario SMTP';
+	$strings['The username used for sending emails'] = 'El nombre de usuario utilizado para enviar correo';
+	$strings['SMTP password'] = 'Contraseña SMTP';
+	$strings['The password used for sending emails'] = 'La contraseña usada para el envío de correo';
+	$strings['Email "from"-name'] = 'Nombre "de" para correo';
+	$strings['The name people will see when receiving emails from BUGS 2'] = 'El nombre de la persona que verán los usuarios cuando reciban un correo de The Bug Genie';
+	$strings['Email "from"-address'] = 'Dirección "de" para correo';
+	$strings['The email-address people will see when receiving emails from BUGS 2'] = 'La cuenta de correo que las personas verán cuando reciban un correo de The Bug Genie';
+	$strings['Email header charset'] = 'Juego de caracteres';
+	$strings['The charset used in outgoing emails'] = 'El conjunto de caracteres utilizado para los correos salientes';
+	$strings['Notification settings'] = 'Opciones de notificación';
+	$strings['Always notify'] = 'Notificar siempre';
+	$strings['Send notification email on my own changes'] = 'Enviar notificación en mis propios cambios';
+	$strings['Only notify me when others are committing changes'] = 'Solo notificarme cuando otros hagan cambios';
+	$strings['Enter an email address, and click "Send test email" to check if the email module is configured correctly'] = 'Ingrese la direcciónd de correo, y haga clic en "Enviar correo de prueba" para verificar que el módulo está configurado correctamente';
+	$strings['The test output will appear below this form'] = 'La respuesta de la prueba aparecerá debajo de este formulario';
+	$strings['Send test email to'] = 'Enviar prueba a';
+	$strings['Send test email'] = 'Enviar correo de prueba';
+	$strings['Test email output'] = 'Respuesta de la prueba';
+	$strings['Test configuration'] = 'Configuración de la prueba';
+	$strings['Mail notification'] = 'Notificación por correo';
+	$strings['Email when user registers'] = 'Correo cuando un usuario registra';
+	$strings['Email to reset password'] = 'Correo al limpiar la contraseña';
+	$strings['Email on issue update'] = 'Correo al actualizar un problema';
+	$strings['Email on new issues'] = 'Correo cuando haya nuevos problemas';
+	$strings['Email when comments are posted'] = 'Correo cuando los comentarios son publicados';
+	$strings['"My account" settings'] = 'Configuración de "Mi cuenta"';
+	$strings['"My account" drop-down settings'] = 'Opciones para el menú "Mi cuenta"';
+	$strings['Email when password is reset'] = 'Correo cuando se restablece la contraseña';
+	$strings['SMTP server timeout'] = 'Tiempo de espera del servidor SMTP';
+	$strings['Timeout in seconds (default is 30)'] = 'Tiempo de espera en segundos (30 por defecto)';
 	$strings['This username does not exist'] = 'Este nombre de usuario no existe';
-	$strings['Username'] = 'Nombre de usuario';
-	$strings['Password'] = 'Contraseña';
-	$strings['If you have forgot your password, enter your username here, and we will send you an email that will allow you to change your username'] = 'Si ha olvidado su contraseña, introduzca su nombre de usuario aquí, y le enviaremos un mensaje de correo electrónico que le permitirá cambiar su contraseña';
 	$strings['Please use the link in the email you received'] = 'Por favor, utilice el enlace en la dirección de correo electrónico que ha recibido';
-	$strings['Issues'] = 'Problemas';
+	$strings['If you have forgot your password, enter your username here, and we will send you an email that will allow you to change your username'] = 'Si ha olvidado su contraseña, introduzca su nombre de usuario aquí, y le enviaremos un mensaje de correo electrónico que le permitirá cambiar su contraseña';
 	$strings['Notify me when someone adds me as their friend'] = 'Avisarme cuando alguien me agrega como amigo';
 	$strings['Notify me when an issue I posted gets updated'] = 'Avisarme cuando un problema que publique es actualizado';
 	$strings['Send me a notification email when I update an issue'] = 'Enviarme un correo cuando actualice un problema';
 	$strings['Notify me when someone comments on an issue I posted'] = 'Avisarme cuando alguien comenta un problema que publiqué';
-	$strings['General'] = 'General';
 	$strings['Outgoing settings'] = 'Configuración saliente';
 	$strings['Incoming settings'] = 'Configuración entrante';
 	$strings['Outgoing emails'] = 'Correos salientes';
 	$strings['These are the settings for outgoing emails, such as notification emails and registration emails.'] = 'Estos son las configuraciones para el correo saliente, tales como la notificación por correo y el correo al registrarse.';
 	$strings['Enable outgoing email notifications'] = 'Habilitar los avisos por correo saliente';
-	$strings['Email "from"-name'] = 'Nombre "de" para correo';
-	$strings['Email "from"-address'] = 'Dirección "de" para correo';
-	$strings['This is the name and email address email notifications from The Bug Genie will be sent from'] = 'Este es el nombre y dirección de correo que The Bug Genie utilizará';
-	$strings['Email header charset'] = 'Juego de caracteres';
-	$strings['The character encoding used in outgoing emails'] = 'La codificación de los caracteres usada en los correos salientes';
+	
+	
+	
+	
+	$strings['The character encoding used in outgoing emails'] = 'El conjunto de caracteres utilizado para los correos salientes';
 	$strings['Queue emails for batch processing'] = 'Correos electrónicos a la cola para el procesamiento en lote';
 	$strings['Send email notifications instantly'] = 'Enviar avisos inmediatamente';
 	$strings['Use email queueing'] = 'Usar cola de correos electrónico';
@@ -32,51 +65,28 @@
 	$strings['Use php settings'] = 'Usar configuración de php';
 	$strings['Use custom settings'] = 'Usar configuración personal';
 	$strings['This setting determines whether The Bug Genie uses the built-in php email function, or a custom configuration'] = 'Esta opción determina si The Bug Genie utiliza funciones de correo propias de php, o una configuración personalizada';
-	$strings['SMTP server address'] = 'Dirección del servidor SMTP';
-	$strings['SMTP address port'] = 'Puerto del servidor SMTP';
-	$strings['SMTP server timeout'] = 'Tiempo de espera del servidor SMTP';
+
+
+
 	$strings['%number_of% seconds'] = '%number_of% segundos';
 	$strings['Connection information for the outgoing email server'] = 'Información de la conexión del servidor para el correo saliente';
 	$strings['Microsoft Exchange server'] = 'Servidor Microsoft Exchange';
 	$strings['For compatibility reasons, specify whether the SMTP server is a Microsoft Exchange server'] = 'Pro motivos de compatibilidad, especifique si el servidor de SMTP es un Microsoft Exchange';
-	$strings['SMTP username'] = 'Nombre de usuario SMTP';
-	$strings['The username used for sending emails'] = 'El nombre de usuario utilizado para enviar correo';
-	$strings['SMTP password'] = 'Contraseña SMTP';
-	$strings['The password used for sending emails'] = 'La contraseña usada para el envío de correo';
+
+
+
+
 	$strings['This section doesn\'t have any settings yet'] = 'Esta sección no tiene opciones todavía';
 	$strings['Click "%save%" to save email notification settings'] = 'Haga clic en "%save%" para guardar las opciones de aviso';
-	$strings['Send test email'] = 'Enviar correo de prueba';
-	$strings['Enter an email address, and click "%send_test_email%" to check if the email module is configured correctly'] = 'Ingrese una dirección de correo, y haga clic en "%send_test_email%" para verificar si el módulo está correctamente configurado';
-	$strings['Configure mail notification module'] = 'Configuración módulo de notificación por correo';
-	$strings['Set up the mail notification module here.'] = 'Defina las opciones del módulo de notificación por correo aquí.';
-	$strings['Set up email- and user notifications from this section.'] = 'Establezca las opciones de correo y notificación a usuarios desde esta sección.';
-	$strings['The address BUGS 2 should use to send emails via'] = 'The address BUGS 2 should use to send emails via';
-	$strings['The port used by the SMTP server (default is 25)'] = 'El puerto utilizado por el servidor SMTP (por defecto es 25)';
-	$strings['Server type'] = 'Tipo de servidor';
-	$strings['Select whether the SMTP server is a MS Exchange server'] = 'Seleccione si el servidor de SMTP es un servidor MS Exchange';
-	$strings['The name people will see when receiving emails from BUGS 2'] = 'El nombre de la persona que verán los usuarios cuando reciban un correo de The Bug Genie';
-	$strings['The email-address people will see when receiving emails from BUGS 2'] = 'La cuenta de correo que las personas verán cuando reciban un correo de The Bug Genie';
-	$strings['The charset used in outgoing emails'] = 'El conjunto de caracteres utilizado para los correos salientes';
-	$strings['Notification settings'] = 'Opciones de notificación';
-	$strings['Always notify'] = 'Notificar siempre';
-	$strings['Send notification email on my own changes'] = 'Send notification email on my own changes';
-	$strings['Only notify me when others are committing changes'] = 'Only notify me when others are committing changes';
-	$strings['Enter an email address, and click "Send test email" to check if the email module is configured correctly'] = 'Enter an email address, and click "Send test email" to check if the email module is configured correctly';
-	$strings['The test output will appear below this form'] = 'The test output will appear below this form';
-	$strings['Send test email to'] = 'Send test email to';
-	$strings['Test email output'] = 'Test email output';
-	$strings['Test configuration'] = 'Test configuration';
-	$strings['Mail notification'] = 'Mail notification';
-	$strings['Email when user registers'] = 'Email when user registers';
-	$strings['Email to reset password'] = 'Email to reset password';
-	$strings['Email on issue update'] = 'Email on issue update';
-	$strings['Email on new issues'] = 'Email on new issues';
-	$strings['Email when comments are posted'] = 'Email when comments are posted';
-	$strings['"My account" settings'] = 'Configuración de "Mi cuenta"';
-	$strings['"My account" drop-down settings'] = 'Opciones para el menú "Mi cuenta"';
-	$strings['Email when password is reset'] = 'Correo cuando se restablece la contraseña';
-	$strings['Timeout in seconds (default is 30)'] = 'Tiempo de espera en segundos (30 por defecto)';
 
+	$strings['Enter an email address, and click "%send_test_email%" to check if the email module is configured correctly'] = 'Ingrese una dirección de correo, y haga clic en "%send_test_email%" para verificar si el módulo está correctamente configurado';
+	$strings['Yes'] = 'Sí';
+	$strings['No'] = 'No';
+	$strings['Save'] = 'Guardar';
+	$strings['Username'] = 'Nombre de usuario';
+	$strings['Password'] = 'Contraseña';
+	$strings['Issues'] = 'Problemas';
+	$strings['General'] = 'General';
 	$strings['Email communication'] = 'Comunicación por correo';
 	$strings['Enables in- and outgoing email functionality'] = 'Habilita la funcionalidad de correo entrante/saliente';
 	$strings['Set up in- and outgoing email communication from this section'] = 'Defina la comunicación para correo entrante y saliente desde esta sección';
@@ -84,23 +94,28 @@
 	$strings['Email when an issue is updated'] = 'Correo cuando un problema es actualizado';
 	$strings['Javascript Mailing'] = 'Correo Javascript';
 	$strings['The email module is not configured for outgoing emails'] = 'El módulo de correo no está configurado para correo saliente';
+
+
 	$strings['Forgot password'] = 'Olvido de contraseña';
 	$strings['Forgot password?'] = '¿Olvidó su contraseña?';
-	$strings['If you have forgot your password, enter your username here, and we will send you an email that will allow you to change your password'] = 'If you have forgot your password, enter your username here, and we will send you an email that will allow you to change your password';
+
 	$strings['Send email'] = 'Enviar correo';
-	$strings['To register, please fill out the information below.'] = 'To register, please fill out the information below.';
-	$strings['Cannot find an email address for this user'] = 'Cannot find an email address for this user';
-	$strings['Forbidden for this username, please contact your administrator'] = 'Forbidden for this username, please contact your administrator';
-	$strings['Please provide a valid setting for SMTP server address'] = 'Por favor defina un valor para la dirección del servidor de correo SMTP';
+	$strings['To register, please fill out the information below.'] = 'Para registrarse, complete la información inferior por favor.';
+	$strings['Cannot find an email address for this user'] = 'No se puede encontrar una dirección de correo para este usuario';
+	$strings['Forbidden for this username, please contact your administrator'] = 'Prohibido para este nombre de usuario, contáctese con su administrador';
 	$strings['Please enter an username'] = 'Ingrese un nombre de usuario por favor';
-	$strings['The email was successfully accepted for delivery'] = 'The email was successfully accepted for delivery';
+	$strings['The email was successfully accepted for delivery'] = 'El correo fue aceptado para el envío';
 	$strings['The email was not sent'] = 'El correo no fue enviado';
 	$strings['Please specify an email address'] = 'Por favor establezca una dirección de correo';
 	$strings['You need to add at least one recipient'] = 'Es necesario agregar al menos un receptor';
-	$strings['You need to add a sender name and address'] = 'You need to add a sender name and address';
-	$strings['Timed out during server conversation'] = 'Timed out during server conversation';
-	$strings['Could not open connection to server %server on port %port%'] = 'Could not open connection to server %server on port %port%';
-	$strings['Not ready to authenticate. (%rv%) Try changing server type'] = 'Not ready to authenticate. (%rv%) Try changing server type';
+	$strings['You need to add a sender name and address'] = 'Es necesario agregar un nombre y una dirección';
+	$strings['Timed out during server conversation'] = 'Se superó el tiempo de espera con el servidor';
+	$strings['Could not open connection to server %server on port %port%'] = 'No se puede establecer la conexión con el servidor %server en el puerto %port%';
+	$strings['Not ready to authenticate. (%rv%) Try changing server type'] = 'No está listo para autenticar. (%rv%) Intente cambiando el tipo de servidor';
 	$strings['Username / password not accepted on server: %rv%'] = 'Nombre de usuario o contraseña no aceptados en el servidor: %rv%';
-	$strings['You are not allowed to send emails through this server. \nThe error was: %rv%'] = 'You are not allowed to send emails through this server. \nThe error was: %rv%';
-	$strings['Did not receive a confirmation message from the mail server.. \nHowever, we received: %rv%'] = 'Did not receive a confirmation message from the mail server.. \nHowever, we received: %rv%';
+	$strings["You are not allowed to send emails through this server. \nThe error was: %rv%"] = "No tiene permitido enviar correo a través de este servidor. \nEl error fue: %rv%";
+	$strings["Did not receive a confirmation message from the mail server.. \nHowever, we received: %rv%"] = "No se recibió un mensaje de confirmación desde el servidor de correo. \nDe todas maneras, recibimos: %rv%";
+	$strings['You are not allowed to send emails through this server. The error was: %rv%']='No tiene permitido enviar correo a través de este servidor. \nEl error fue: %rv%';
+	$strings['Did not receive a confirmation message from the mail server.. However, we received: %rv%']='No se recibió un mensaje de confirmación desde el servidor de correo. \nDe todas maneras, recibimos: %rv%';
+	$strings['The character encoding used in outgoing emails'] = 'La codificación de los caracteres usada en los correos salientes';
+	$strings['This is the name and email address email notifications from The Bug Genie will be sent from'] = 'Este es el nombre y dirección de correo que The Bug Genie utilizará';
