0927274cc3e4d175b8ab070fad7c4d383af0a35c
lsproc <kentphilip@gmail.com>
2011-01-12 20:55:15 +0000

    Don't allow access to the login page if we are logged in

--- modules/main/templates/login.html.php
+++ modules/main/templates/login.html.php
@@ -2,8 +2,21 @@
 
 	TBGContext::loadLibrary('ui');
 
+	if (TBGContext::getUser()->isGuest()):
 ?>
 
 <script>
 	showFadedBackdrop('<?php echo make_url('get_partial_for_backdrop', array_merge(array('key' => 'login'), $options)); ?>');
 </script>
+
+<?php
+	else:
+?>
+
+<div class="rounded_box green borderless loggedindiv" >
+	<?php echo __('You are already logged in'); ?>
+</div>
+
+<?php 
+	endif;
+?>
