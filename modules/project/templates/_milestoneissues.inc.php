78ac5821833dd174a97a67422e564f104f5e50d1
lsproc <kentphilip@gmail.com>
2011-01-23 10:43:31 +0000

    Make blocking issues (which are open) red on the roadmap)

--- modules/project/templates/_milestoneissues.inc.php
+++ modules/project/templates/_milestoneissues.inc.php
@@ -1,5 +1,5 @@
 <?php foreach ($milestone->getIssues() as $issue): ?>
-	<div class="roadmap_issue<?php if ($issue->isClosed()): ?> faded_out issue_closed<?php endif; ?>">
+	<div class="roadmap_issue<?php if ($issue->isClosed()): ?> faded_out issue_closed<?php elseif ($issue->isBlocking()): ?> blocking<?php endif; ?>">
 		<div class="issue_status"><div style="border: 1px solid #AAA; background-color: <?php echo ($issue->getStatus() instanceof TBGStatus) ? $issue->getStatus()->getColor() : '#FFF'; ?>; font-size: 1px; width: 13px; height: 13px;" title="<?php echo ($issue->getStatus() instanceof TBGStatus) ? $issue->getStatus()->getName() : ''; ?>">&nbsp;</div></div>
 		<?php echo link_tag(make_url('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo())), $issue->getFormattedIssueNo(true, true) . ' - <span class="issue_title">' . $issue->getTitle() . '</span>'); ?>
 		<?php if ($milestone->isSprint()): ?>
