75700cf60e761cd4e1a7eed47d1d3f8c0f575467
zegenie <zegenie@gmail.com>
2011-03-07 13:15:53 +0000

    translate report issue text, also add breadcrumb links for report issue page

--- modules/main/templates/reportissue.html.php
+++ modules/main/templates/reportissue.html.php
@@ -1,7 +1,7 @@
 <?php 
 
-	$tbg_response->addBreadcrumb(__('Create new issue'));
-	$tbg_response->setTitle('Report an issue');
+	$tbg_response->addBreadcrumb(__('Report an issue'), make_url('project_reportissue', array('project_key' => TBGContext::getCurrentProject()->getKey())), tbg_get_breadcrumblinks('project_summary', TBGContext::getCurrentProject()));
+	$tbg_response->setTitle(__('Report an issue'));
 	$tbg_response->addJavascript('reportissue.js');
 	
 	if (TBGContext::getCurrentProject()->isLocked() == true)
