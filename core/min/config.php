e09ea39958d5e848853bc76e87a74a00a2d23845
Philip Kent <kentphilip@gmail.com>
2011-06-25 23:54:32 +0100

    Allow themes to have their own configuration file, where the themes can specify CSS files and JS files to load which aren't the default ones.
    Add an option for CSS and JS to be loaded without minify or the
    content server.
    
    Fix the droid font in a number of instances

--- core/min/config.php
+++ core/min/config.php
@@ -72,7 +72,7 @@ $min_cacheFileLocking = true;
  * move all @imports to the top of the output. Note that moving @imports could 
  * affect CSS values (which is why this option is disabled by default).
  */
-$min_serveOptions['bubbleCssImports'] = false;
+$min_serveOptions['bubbleCssImports'] = true;
 
 
 /**
---
+++ thebuggenie/themes/oxygen/theme.php
@@ -0,0 +1,8 @@
+<?php
+
+	/**
+	 * Configuration for theme
+	 */
+
+	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/fonts/droidsans/stylesheet.css', false);
+	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/fonts/droidsansmono/stylesheet.css', false);
