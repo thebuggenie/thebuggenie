096735fd1d4baa89047574c0e27c9d94a8f308b5
Philip Kent <kentphilip@gmail.com>
2011-07-24 14:09:46 +0100

    Fix the percentage completed display in csv export, fixes #783

--- modules/search/templates/_results_normal_csv.inc.php
+++ modules/search/templates/_results_normal_csv.inc.php
@@ -95,14 +95,7 @@ else
 
 unset($temp);
 
-if ($issue->getPercentComplete() == '')
-{
-	$percent = '0%';
-}
-else
-{
-	$percent = $issue->getPercentComplete();
-}
+$percent = $issue->getPercentCompleted().'%';
 
 ?>
 "<?php echo $issue->getProject()->getName(); ?>","<?php echo $issue->getFormattedIssueNo(); ?>","<?php echo strip_tags($issue->getTitle()); ?>","<?php echo $assignee; ?>","<?php echo $status; ?>","<?php echo $category; ?>","<?php echo $priority; ?>","<?php echo $reproducability; ?>","<?php echo $severity; ?>","<?php echo $resolution; ?>","<?php echo $milestone; ?>","<?php echo tbg_formatTime($issue->getLastUpdatedTime(), 21); ?>","<?php echo $percent; ?>","<?php echo $issue->getFormattedTime($issue->getEstimatedTime()); ?>","<?php echo $issue->getFormattedTime($issue->getSpentTime());?>","<?php echo $issue->getUserpain(); ?>","<?php echo $issue->getVotes(); ?>"
