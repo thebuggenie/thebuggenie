40408f96eaa83659553682c7cbab3410655df810
Philip Kent <kentphilip@gmail.com>
2011-07-26 12:22:28 +0100

    Remove some PHP from CSS and restructure

--- thebuggenie/themes/oxygen/theme.php
+++ thebuggenie/themes/oxygen/theme.php
@@ -6,6 +6,7 @@
 
 	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/fonts/droidsans/stylesheet.css', false);
 	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/fonts/droidsansmono/stylesheet.css', false);
-	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/css/oxygen_tablekit.css', false);
 	TBGContext::getResponse()->addStylesheet('oxygen.css');
-	TBGContext::getResponse()->addStylesheet('markitup.css');
+	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/css/oxygen_markitup.css', false);
+	TBGContext::getResponse()->addStylesheet('oxygen_tablekit.css');
+	
