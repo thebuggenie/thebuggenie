983fcb6187434daea144191f6730e22053334b4b
Philip Kent <kentphilip@gmail.com>
2011-07-28 11:55:46 +0100

    In installer, if the core directory is not writable it will try to make the cache directory anyway, this can lead to an error instead of it being caught, so check for access first.

--- modules/installation/classes/actions.class.php
+++ modules/installation/classes/actions.class.php
@@ -86,13 +86,9 @@
 				$this->base_folder_perm_ok = false;
 				$this->all_well = false;
 			}
-
-            try
-            {
-			    if (!file_exists(THEBUGGENIE_CORE_PATH . 'cache')) mkdir(THEBUGGENIE_CORE_PATH . 'cache');
-			    if (!file_exists(THEBUGGENIE_CORE_PATH . 'cache' . DS . 'B2DB')) mkdir(THEBUGGENIE_CORE_PATH . 'cache' . DS . 'B2DB');
-            }
-            catch (Exception $e) {}
+			
+			if (!file_exists(THEBUGGENIE_CORE_PATH . 'cache') && is_writable(THEBUGGENIE_CORE_PATH)) mkdir(THEBUGGENIE_CORE_PATH . 'cache');
+			if (!file_exists(THEBUGGENIE_CORE_PATH . 'cache' . DS . 'B2DB') && is_writable(THEBUGGENIE_CORE_PATH . 'cache')) mkdir(THEBUGGENIE_CORE_PATH . 'cache' . DS . 'B2DB');
 			
 			if (!is_writable(THEBUGGENIE_CORE_PATH . 'cache' . DS) || !is_writable(THEBUGGENIE_CORE_PATH . 'cache' . DS .'B2DB' . DS))
 			{
