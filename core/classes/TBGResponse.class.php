e9d56a62564f6e60811836a4572e1da745e4a682
Daniel André Eikeland <dae@elyndir.(none)>
2011-07-28 09:37:55 +0200

    indentation fixes

--- core/classes/TBGResponse.class.php
+++ core/classes/TBGResponse.class.php
@@ -455,11 +455,11 @@
 		{
 			header("HTTP/1.0 ".$this->_http_status);
 			/* headers to stop caching in browsers and proxies */
-			header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-			header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
-			header ("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-			header ("Pragma: no-cache"); // HTTP/1.0
-			header ("Content-Type: " . $this->_content_type . "; charset=" . TBGContext::getI18n()->getCharset());
+			header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+			header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+			header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+			header("Pragma: no-cache"); // HTTP/1.0
+			header("Content-Type: " . $this->_content_type . "; charset=" . TBGContext::getI18n()->getCharset());
 
 			foreach ($this->_headers as $header)
 			{
