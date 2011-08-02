4a88ea2f88fc7d98db9bd563078b0204a4da232c
Philip Kent <kentphilip@gmail.com>
2011-08-02 16:20:26 +0100

    If a milestone has no issues assigned, don't say there are issues you can not access

--- modules/project/templates/_milestoneissues.inc.php
+++ modules/project/templates/_milestoneissues.inc.php
@@ -1,7 +1,6 @@
 <?php $c = 0; ?>
 <?php foreach ($milestone->getIssues() as $issue): ?>
 	<?php if ($issue->hasAccess()): ?>
-		<?php $c++; ?>
 		<div class="roadmap_issue<?php if ($issue->isClosed()): ?> faded_out issue_closed<?php elseif ($issue->isBlocking()): ?> blocking<?php endif; ?>">
 			<div class="issue_status"><div style="border: 1px solid #AAA; background-color: <?php echo ($issue->getStatus() instanceof TBGStatus) ? $issue->getStatus()->getColor() : '#FFF'; ?>; font-size: 1px; width: 13px; height: 13px;" title="<?php echo ($issue->getStatus() instanceof TBGStatus) ? $issue->getStatus()->getName() : ''; ?>">&nbsp;</div></div>
 			<?php if ($issue->isAssigned()): ?>
@@ -18,8 +17,10 @@
 				<div class="issue_points"><?php echo __('%pts% points', array('%pts%' => $issue->getEstimatedPoints())); ?></div>
 			<?php endif; ?>
 		</div>
+	<?php else: ?>
+		<?php $c++; ?>
 	<?php endif; ?>
 <?php endforeach; ?>
-<?php if ($c == 0): ?>
+<?php if ($c > 0): ?>
 <span class="faded_out"><?php echo __('This milestone also includes issues you do not have access to'); ?></span>
 <?php endif; ?>
