5017fa66e009bb575bbade60f7695c553adcac1a
Philip Kent <kentphilip@gmail.com>
2011-08-02 17:05:25 +0100

    Set default views

--- core/classes/B2DB/TBGDashboardViewsTable.class.php
+++ core/classes/B2DB/TBGDashboardViewsTable.class.php
@@ -104,6 +104,15 @@
 					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_VIEW_PREDEFINED_SEARCH, 'id' => TBGContext::PREDEFINED_SEARCH_TEAM_ASSIGNED_OPEN_ISSUES));
 					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_VIEW_LOGGED_ACTION, 'id' => 0));
 					break;
+				case self::TYPE_PROJECT:
+					$this->clearViews($target_id);
+					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_PROJECT_INFO, 'id' => 0));
+					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_PROJECT_TEAM, 'id' => 0));
+					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_PROJECT_SUBPROJECTS, 'id' => 0));
+					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_PROJECT_CLIENT, 'id' => 0));
+					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_PROJECT_LAST15, 'id' => 0));
+					$this->addView($target_id, $target_type, array('type' => TBGDashboard::DASHBOARD_PROJECT_STATISTICS, 'id' => 0));
+					break;
 			}
 
 		}
