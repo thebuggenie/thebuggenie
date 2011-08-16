ffde6812f45e04b0607bef949093e0276fa0620e
Philip Kent <kentphilip@gmail.com>
2011-08-16 16:11:55 +0100

    Switch to Open Sans, and load Droid Sans Mono from Google

--- thebuggenie/themes/oxygen/theme.php
+++ thebuggenie/themes/oxygen/theme.php
@@ -4,8 +4,6 @@
 	 * Configuration for theme
 	 */
 
-	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/fonts/droidsans/stylesheet.css', false);
-	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/fonts/droidsansmono/stylesheet.css', false);
 	TBGContext::getResponse()->addStylesheet('oxygen.css');
 	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/css/oxygen_markitup.css', false);
 	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/css/oxygen_tablekit.css', false);
