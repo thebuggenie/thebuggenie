78521227688dcc2ca83ee486eb724db9958f57fa
Daniel André Eikeland <zegenie@gmail.com>
2011-09-19 13:02:08 +0200

    add tiny bit of padding so the buttons don't look so close together

--- modules/mailing/templates/_incomingemailaccount.inc.php
+++ modules/mailing/templates/_incomingemailaccount.inc.php
@@ -1,7 +1,7 @@
 <div id="incoming_email_account_<?php echo $account->getID(); ?>">
 	<h5>
-		<div class="button button-silver" style="float: right; margin-top: 5px;"><span onclick="TBG.Main.Helpers.Backdrop.show('<?php echo make_url('get_partial_for_backdrop', array('key' => 'mailing_editincomingemailaccount', 'project_id' => $account->getProject()->getId(), 'account_id' => $account->getID())); ?>');"><?php echo __('Edit'); ?></span></div>
-		<div class="button button-green" style="float: right; margin-top: 5px;"><span onclick="TBG.Modules.mailing.checkIncomingAccount('<?php echo make_url('mailing_check_account', array('account_id' => $account->getID())); ?>');"><?php echo __('Check now'); ?></span></div>
+		<div class="button button-silver" style="float: right; margin: 10px 0 0 5px;"><span onclick="TBG.Main.Helpers.Backdrop.show('<?php echo make_url('get_partial_for_backdrop', array('key' => 'mailing_editincomingemailaccount', 'project_id' => $account->getProject()->getId(), 'account_id' => $account->getID())); ?>');"><?php echo __('Edit'); ?></span></div>
+		<div class="button button-green" style="float: right; margin-top: 10px;"><span onclick="TBG.Modules.mailing.checkIncomingAccount('<?php echo make_url('mailing_check_account', array('account_id' => $account->getID())); ?>');"><?php echo __('Check now'); ?></span></div>
 		<span id="mailing_account_<?php echo $account->getID(); ?>_name"><?php echo $account->getName(); ?></span> <span class="faded_out" style="font-size: 0.8em; font-weight: normal;"><?php echo $account->getServer(); ?></span>
 		<div style="font-size: 0.9em; font-weight: normal;">
 			<span><?php echo __('Last checked: %time%', array('%time%' => '')); ?><span id="mailing_account_<?php echo $account->getID(); ?>_time" style="font-style: italic;"><?php echo ($account->getTimeLastFetched()) ? tbg_formatTime($account->getTimeLastFetched(), 6) : __('%last_checked% never', array('%last_checked%' => '')); ?></span>.</span>
