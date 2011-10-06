a039d1c26fa04d46dbf98255361b82195bfd05be
Daniel André Eikeland <zegenie@gmail.com>
2011-10-06 15:29:07 +0200

    create the dashboardviews table on upgrade

--- modules/installation/classes/actions.class.php
+++ modules/installation/classes/actions.class.php
@@ -449,6 +449,7 @@
 
 			TBGProjectsTable::getTable()->upgrade(TBGProjectsTable3dot1::getTable());
 			TBGBuildsTable::getTable()->upgrade(TBGBuildsTable3dot1::getTable());
+			TBGDashboardViewsTable::getTable()->create();
 
 			TBGSettings::saveSetting(TBGSettings::SETTING_ICONSET, TBGSettings::get(TBGSettings::SETTING_THEME_NAME));
 			
