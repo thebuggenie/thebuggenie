403f3c7d16e19876ec7a3983a7c087dd833e9e3d
Daniel André Eikeland <zegenie@gmail.com>
2011-10-28 16:24:25 +0200

    merge more js and css files

--- thebuggenie/themes/oxygen/theme.php
+++ thebuggenie/themes/oxygen/theme.php
@@ -5,6 +5,4 @@
 	 */
 
 	TBGContext::getResponse()->addStylesheet('oxygen.css');
-	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/themes/oxygen/markitup.css', false);
-	TBGContext::getResponse()->addStylesheet(TBGContext::getStrippedTBGPath().'/themes/oxygen/tablekit.css', false);
 	
