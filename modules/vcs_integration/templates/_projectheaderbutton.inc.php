1a3602a9bb7ce5beab60a3d693adc8e7362c9db0
Daniel André Eikeland <zegenie@gmail.com>
2011-10-28 17:25:00 +0200

    fix blue source code button being double wrapped

--- modules/vcs_integration/templates/_projectheaderbutton.inc.php
+++ modules/vcs_integration/templates/_projectheaderbutton.inc.php
@@ -7,7 +7,7 @@
 	
 	if (TBGContext::getModule('vcs_integration')->getSetting('vcs_mode_' . TBGContext::getCurrentProject()->getID()) != TBGVCSIntegration::MODE_DISABLED)
 	{
-		echo '<div class="button button-blue"><a href="'.$link_repo.'" target="_blank">'.image_tag('cfg_icon_vcs_integration.png', null, false, 'vcs_integration').__('Source code').'</a></div>';
+		echo '<a href="'.$link_repo.'" target="_blank" class="button button-blue">'.image_tag('cfg_icon_vcs_integration.png', null, false, 'vcs_integration').__('Source code').'</a>';
 	}
 
 ?>
