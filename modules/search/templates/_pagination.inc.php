de56a727f95d8b3c82545587a081dd28048c2736
Daniel André Eikeland <zegenie@gmail.com>
2011-11-03 10:41:39 +0100

    add arrows on next/prev/first/last page buttons

--- modules/search/templates/_pagination.inc.php
+++ modules/search/templates/_pagination.inc.php
@@ -2,9 +2,9 @@
 	<div class="rounded_box white borderless" style="width: 500px; margin: 10px auto 10px auto; padding: 3px 5px 3px 5px; font-size: 13px; text-align: center;">
 		<?php if ($currentpage > 1): ?>
 			<?php if ($currentpage > 2): ?>
-				<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', 0);"><?php echo __('First page'); ?></button>
+			<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', 0);">&larrb; <?php echo __('First page'); ?></button>
 			<?php endif; ?>
-			<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', <?php echo ($currentpage - 2) * $ipp; ?>);"><?php echo __('Previous page'); ?></button>
+			<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', <?php echo ($currentpage - 2) * $ipp; ?>);">&laquo; <?php echo __('Previous page'); ?></button>
 		<?php endif; ?>
 		<?php for ($cc = 1; $cc <= $pagecount; $cc++): ?>
 			<?php if ($cc == $currentpage): ?>
@@ -14,9 +14,9 @@
 			<?php endif; ?>
 		<?php endfor; ?>
 		<?php if ($currentpage < $pagecount): ?>
-			<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', <?php echo $currentpage * $ipp; ?>);"><?php echo __('Next page'); ?></button>
+			<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', <?php echo $currentpage * $ipp; ?>);"><?php echo __('Next page'); ?> &raquo;</button>
 			<?php if ($currentpage < $pagecount - 1): ?>
-				<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', <?php echo ($pagecount - 1) * $ipp; ?>);"><?php echo __('Last page'); ?></button>
+				<button class="button button-silver" onclick="TBG.Search.toPage('<?php echo $route; ?>', '<?php echo $parameters; ?>', <?php echo ($pagecount - 1) * $ipp; ?>);"><?php echo __('Last page'); ?> &rarrb;</button>
 			<?php endif; ?>
 		<?php endif; ?>
 		<?php echo image_tag('spinning_20.gif', array('id' => 'paging_spinning', 'style' => 'display: none; margin: 0 0 -6px 5px;')); ?>
