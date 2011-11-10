4ec1646614c9ec4bb9ecdf46acc3d2646032b97c
pb30 <pb30@pb30.com>
2011-11-10 10:34:37 -0600

    Set global JS libraries as important

--- core/lib/common.inc.php
+++ core/lib/common.inc.php
@@ -338,8 +338,8 @@
 	function tbg_get_javascripts()
 	{
 		$tbg_response = TBGContext::getResponse();
-		$tbg_response->addJavascript('jquery-1.6.2.min.js');
-		$tbg_response->addJavascript('prototype.js');
+		$tbg_response->addJavascript('prototype.js', true, true);
+		$tbg_response->addJavascript('jquery-1.6.2.min.js', true, true);
 		$tbg_response->addJavascript('builder.js');
 		$tbg_response->addJavascript('effects.js');
 		$tbg_response->addJavascript('dragdrop.js');
