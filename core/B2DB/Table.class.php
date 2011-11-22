7d221577f7cdb5b898c5afcf35bc7be94ea30c96
Daniel André Eikeland <zegenie@gmail.com>
2011-11-22 16:12:05 +0100

    fix float issue

--- core/B2DB/Table.class.php
+++ core/B2DB/Table.class.php
@@ -590,7 +590,7 @@
 		{
 			switch ($type) {
 				case 'float':
-					return settype(gmp_strval($value));
+					return ($value) ? settype(gmp_strval($value)) : $value;
 				case 'varchar':
 				case 'text':
 					return (string) $value;
