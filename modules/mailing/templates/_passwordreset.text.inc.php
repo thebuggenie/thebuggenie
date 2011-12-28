e1dc8bd3292040158d7ff1d0a568a58265c6c4cb
Philip Kent <kentphilip@gmail.com>
2011-12-28 18:25:53 +0000

    Password in forgotten password email is set using template parameters, not email replaceable values. As we are using the wrong one, it made the forgot password feature a bit useless.

--- modules/mailing/templates/_passwordreset.text.inc.php
+++ modules/mailing/templates/_passwordreset.text.inc.php
@@ -2,4 +2,4 @@ Hi, %user_buddyname%!
 
 A request was made to reset your password for your user account at %thebuggenie_url%.
 The new password has been saved. Click this link: %thebuggenie_url%
-and log in with the username %user_username% and the password %password%
+and log in with the username %user_username% and the password <?php echo $password; ?>
