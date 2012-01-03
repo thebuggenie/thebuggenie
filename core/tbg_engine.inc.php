a0c6aae769be3df51f1c02fef3709abe8189b3a9
Philip Kent <kentphilip@gmail.com>
2012-01-03 18:07:54 +0000

    Update bug report URL

--- core/tbg_engine.inc.php
+++ core/tbg_engine.inc.php
@@ -330,7 +330,7 @@
 							No login is required - but if you have a username and password entering it below will post the issue with your username, allowing you to follow its progress.
 						</div>
 						<br>
-						<form action=\"http://thebuggenie.com/thebuggenie/thebuggenie/issues/new/bugreport\" target=\"_new\" method=\"post\">
+						<form action=\"http://issues.thebuggenie.com/thebuggenie/issues/new/bugreport\" target=\"_new\" method=\"post\">
 							<label for=\"username\">Username <span>(optional)</span></label>
 							<input type=\"text\" name=\"tbg3_username\" id=\"username\">
 							<br style=\"clear: both;\">
