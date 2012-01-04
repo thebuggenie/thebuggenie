c3e5556f2326dbf57a55ffc70576c1e622d8b7b3
Philip Kent <kentphilip@gmail.com>
2012-01-04 00:01:33 +0000

    Restyle installer, looks a lot better now

--- modules/installation/templates/upgrade.html.php
+++ modules/installation/templates/upgrade.html.php
@@ -10,15 +10,13 @@
 		</ul>
 		<h4>If this software has turned out to be valuable to you and/or your business - please consider supporting it.</h4>
 		More information about supporting The Bug Genie development can be found here:
-		<a target="_blank" href="http://www.thebuggenie.com/support.php">http://www.thebuggenie.com/support.php</a> <i>(opens in a new window)</i>
+		<a target="_blank" href="http://www.thebuggenie.com/support">http://www.thebuggenie.com/support</a> <i>(opens in a new window)</i>
 	</div>
 <?php endif; ?>
 <div class="installation_box">
 	<?php if ($upgrade_available): ?>
 		<h2>You are performing the following upgrade: <?php echo $current_version; ?>.x => 3.2<br>
-			Make a backup of your installation before you continue!</h2>
-		<br>
-		<br>
+		<span class="smaller">Make a backup of your installation before you continue!</span></h2>
 		<?php if (isset($permissions_ok) && $permissions_ok): ?>
 			<form accept-charset="utf-8" action="<?php echo make_url('upgrade'); ?>" method="post">
 				<input type="hidden" name="perform_upgrade" value="1">
@@ -26,7 +24,7 @@
 				<label for="confirm_backup" style="font-weight: bold; font-size: 14px;">
 					I have made a backup, and will be solely responsible if I have chosen not to do so
 					<div style="font-weight: normal; font-size: 11px; margin-left: 25px;">And even if I have made a backup I know there is no 100% guarantee this will work</div></label>&nbsp;&nbsp;<br>
-				<input type="submit" style="margin-top: 15px;" value="Perform upgrade" id="start_upgrade" disabled="disabled">
+				<input type="submit" value="Perform upgrade" id="start_upgrade" disabled="disabled">
 			</form>
 		<?php else: ?>
 			<div class="installation_prerequisites prereq_fail">
