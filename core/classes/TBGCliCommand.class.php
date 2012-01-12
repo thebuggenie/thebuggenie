d787cfbcb56f8cc4840534885d04ef290dde19ff
Daniel André Eikeland <zegenie@gmail.com>
2012-01-12 08:38:23 +0100

    support non-colors on win platform

--- core/classes/TBGCliCommand.class.php
+++ core/classes/TBGCliCommand.class.php
@@ -240,12 +240,19 @@
 
 		public static function cli_echo($text, $color = 'white', $style = null)
 		{
-			$fg_colors = array('black' => 29, 'red' => 31, 'green' => 32, 'yellow' => 33, 'blue' => 34, 'magenta' => 35, 'cyan' => 36, 'white' => 37);
-			$op_format = array('bold' => 1, 'underline' => 4, 'blink' => 5, 'reverse' => 7, 'conceal' => 8);
+			if (PHP_OS == 'Windows')
+			{
+				$return_text = $text;
+			}
+			else
+			{
+				$fg_colors = array('black' => 29, 'red' => 31, 'green' => 32, 'yellow' => 33, 'blue' => 34, 'magenta' => 35, 'cyan' => 36, 'white' => 37);
+				$op_format = array('bold' => 1, 'underline' => 4, 'blink' => 5, 'reverse' => 7, 'conceal' => 8);
 
-			$return_text = "\033[" . $fg_colors[$color];
-			$return_text .= ($style !== null && array_key_exists($style, $op_format)) ? ";" . $op_format[$style] : '';
-			$return_text .= "m" . $text . "\033[0m";
+				$return_text = "\033[" . $fg_colors[$color];
+				$return_text .= ($style !== null && array_key_exists($style, $op_format)) ? ";" . $op_format[$style] : '';
+				$return_text .= "m" . $text . "\033[0m";
+			}
 
 			echo $return_text;
 		}
