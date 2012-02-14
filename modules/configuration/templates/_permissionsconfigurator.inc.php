21f68bf0267e8227e19c363797f5676d7a40144c
Daniel André Eikeland <zegenie@gmail.com>
2012-02-14 14:03:35 +0100

    some permissions configuration item cleanups

--- modules/configuration/templates/_permissionsconfigurator.inc.php
+++ modules/configuration/templates/_permissionsconfigurator.inc.php
@@ -1,27 +1,3 @@
-<div class="rounded_box borderless mediumgrey" style="margin: 5px 0px 10px 0px; min-height: 85px;">
-	<div class="header_div smaller" style="clear: both; margin: 0 0 5px 0;"><?php echo __('Icon legend:'); ?></div>
-	<div style="clear: both;">
-		<?php echo image_tag('icon_project_permissions.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Show more detailed permissions for this permission group'); ?></span>
-		<?php echo image_tag('cfg_icon_permissions.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Set permissions'); ?></span>
-	</div>
-	<div class="header_div smaller" style="clear: both; margin: 0 0 5px 0; padding-top: 10px;"><?php echo __('Permissions icon legend:'); ?></div>
-	<div style="clear: both;">
-		<?php echo image_tag('permission_unset_ok.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Not set (permissive system setting)'); ?></span>
-		<?php echo image_tag('permission_unset_denied.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Not set (restrictive system setting)'); ?></span>
-		<?php echo image_tag('permission_set_unset.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Not set (uses global permission)'); ?></span>
-	</div>
-	<div style="clear: both;">
-		<?php echo image_tag('permission_set_ok.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Allowed'); ?></span>
-		<?php echo image_tag('permission_set_denied.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Denied'); ?></span>
-	</div>
-	<div style="clear: both; padding: 10px 0 5px 5px;">
-		<?php if (!$user_id): ?>
-			<?php echo tbg_parse_text(__("Edit all global, group and team permissions from this page - user-specific permissions are handled from the [[TBG:configure_users|user configuration page]]. The Bug Genie permissions are thoroughly explained in [[ConfigurePermissions]] in the wiki - look it up if you're ever stuck.", array(), true)); ?>
-		<?php else: ?>
-			<?php echo tbg_parse_text(__("Edit all user-specific permissions from this page - global, group and team permissions are handled from the [[TBG:configure_permissions|permissions configuration page]]. The Bug Genie permissions are thoroughly explained in [[ConfigurePermissions]] in the wiki - look it up if you're ever stuck.", array(), true)); ?>
-		<?php endif; ?>
-	</div>
-</div>
 <div style="margin: 10px 0 0 0; clear: both; height: 30px;" class="tab_menu">
 	<ul id="permissions_<?php echo $base_id; ?>_tabs">
 		<li class="selected" id="<?php echo $base_id; ?>_tab_general"><a onclick="TBG.Main.Helpers.tabSwitcher('<?php echo $base_id; ?>_tab_general', 'permissions_<?php echo $base_id; ?>_tabs');" href="javascript:void(0);"><?php echo __('General permissions'); ?></a></li>
--- modules/configuration/templates/configurepermissions.html.php
+++ modules/configuration/templates/configurepermissions.html.php
@@ -9,7 +9,37 @@
 		<td valign="top" style="padding-left: 15px;">
 			<div style="width: 788px;">
 				<h3><?php echo __('Configure permissions'); ?></h3>
-				<div id="config_permissions" class="config_permissions">
+				<div class="config_permissions rounded_box borderless mediumgrey" style="margin: 0 0 10px 10px; min-height: 85px; width: 330px; float: right;">
+					<div class="header_div smaller" style="clear: both; margin: 0 0 5px 0;"><?php echo __('Icon legend:'); ?></div>
+					<div style="clear: both;">
+						<?php echo image_tag('icon_project_permissions.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Show more detailed permissions'); ?></span>
+					</div>
+					<div style="clear: both;">
+						<?php echo image_tag('cfg_icon_permissions.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Set permissions'); ?></span>
+					</div>
+					<div class="header_div smaller" style="clear: both; margin: 0 0 5px 0; padding-top: 10px;"><?php echo __('Permissions icon legend:'); ?></div>
+					<div style="clear: both;">
+						<?php echo image_tag('permission_unset_ok.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Not set (permissive system setting)'); ?></span>
+					</div>
+					<div style="clear: both;">
+						<?php echo image_tag('permission_unset_denied.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Not set (restrictive system setting)'); ?></span>
+					</div>
+					<div style="clear: both;">
+						<?php echo image_tag('permission_set_unset.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Not set (uses global permission)'); ?></span>
+					</div>
+					<div style="clear: both;">
+						<?php echo image_tag('permission_set_ok.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Allowed'); ?></span>
+					</div>
+					<div style="clear: both;">
+						<?php echo image_tag('permission_set_denied.png', array('style' => 'float: left; margin: 0 5px 0 10px;')); ?><span style="float: left;"><?php echo __('Denied'); ?></span>
+					</div>
+				</div>
+				<p>
+					<?php echo tbg_parse_text(__("Edit all global group and team permissions from this page - user-specific permissions are handled from the [[TBG:configure_users|user configuration page]], and permissions for specific issue fields are available from the [[TBG:configure_issuefields|issue fields configuration page]].", array(), true)); ?><br>
+					<br>
+					<?php echo tbg_parse_text(__("The Bug Genie permissions are thoroughly explained in [[ConfigurePermissions]] in the wiki - look it up if you're ever stuck.", array(), true)); ?>
+				</p>
+				<div id="config_permissions" class="" style="clear: both;">
 					<?php include_component('configuration/permissionsconfigurator', array('access_level' => $access_level, 'base_id' => 'configurator')); ?>
 				</div>
 			</div>
