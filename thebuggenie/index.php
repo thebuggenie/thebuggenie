6ab08beacff8f409b1971a1cbe7cfd4441ea22f8
Joshua <joshua.anthony@habanero.se>
2012-03-29 13:23:43 +0300

    Fix for #1349 Define GESHI_ROOT in index.php - the define statement seems to have been lost somewhere along the way.

--- thebuggenie/index.php
+++ thebuggenie/index.php
@@ -10,6 +10,7 @@
 	defined('THEBUGGENIE_PUBLIC_FOLDER_NAME') || define('THEBUGGENIE_PUBLIC_FOLDER_NAME', mb_substr($path, strrpos($path, DS) + 1));
 	defined('B2DB_BASEPATH') || define ('B2DB_BASEPATH', THEBUGGENIE_CORE_PATH . 'B2DB' . DS);
 	defined('B2DB_CACHEPATH') || define ('B2DB_CACHEPATH', THEBUGGENIE_CORE_PATH . 'cache' . DS . 'B2DB' . DS);
+	defined('GESHI_ROOT') || define('GESHI_ROOT', THEBUGGENIE_CORE_PATH . 'geshi' . DS);
 	
 	// Include the "engine" script, which initializes and sets up stuff
 	require THEBUGGENIE_CORE_PATH . 'bootstrap.php';
