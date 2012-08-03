8e323917211f3567d369b430c4ce8db54d5ecbb9
Daniel André Eikeland <zegenie@gmail.com>
2012-08-03 10:22:36 +0200

    version bump

--- core/classes/TBGSettings.class.php
+++ core/classes/TBGSettings.class.php
@@ -102,8 +102,8 @@
 
 		protected static $_ver_mj = 3;
 		protected static $_ver_mn = 2;
-		protected static $_ver_rev = '1';
-		protected static $_ver_name = "Bolstadøyri";
+		protected static $_ver_rev = '2';
+		protected static $_ver_name = "Ystabøhamn";
 		protected static $_defaultscope = null;
 		protected static $_settings = null;
 		protected static $_loadedsettings = array();
