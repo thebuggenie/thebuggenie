e3911371dac95072d953b187fda867b1bb2cca65
Luciano Ziegler <emaildoluc@yahoo.com.br>
2012-08-12 11:14:34 -0500

    Update modules/mailing/templates/_editincomingemailaccount.inc.php

--- modules/mailing/templates/_editincomingemailaccount.inc.php
+++ modules/mailing/templates/_editincomingemailaccount.inc.php
@@ -43,11 +43,11 @@
 					<td><label for="account_port"><?php echo __('Port number:'); ?></label></td>
 					<td><input type="text" name="port" id="account_port" style="width: 50px;" value="<?php echo $account->getPort(); ?>"></td>
 				</tr>
-                <tr>
+				<tr>
 					<td><label for="account_foldername"><?php echo __('Email folder name:'); ?></label></td>
 					<td><input type="text" name="folder" id="account_foldername" style="width: 200px;" value="<?php echo $account->getFoldername(); ?>"></td>
 				</tr>
-                                <tr>
+				<tr>
 					<td>&nbsp;</td>
 					<td class="faded_out"><?php echo __('Enter folder name to read from. Leave blank for default (INBOX)'); ?></td>
 				</tr>
