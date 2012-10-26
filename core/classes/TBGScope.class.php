a7391158e9215487bc2ef6e911d4750e8cced694
Wolfgang <wolfgang@gassler.org>
2012-10-26 16:07:22 +0200

    fixed issue 1802 - parse_url with php < 5.4.7

--- core/classes/TBGScope.class.php
+++ core/classes/TBGScope.class.php
@@ -264,12 +264,18 @@
 
 		public function getCurrentHostname($clean = false)
 		{
-			if ($clean)
-			{
-				$url = parse_url($this->_hostname);
-				return $url['host'];
-			}
-			return $this->_hostname;
+                        if ($clean)
+                        {
+                                // a scheme is needed before php 5.4.7
+                                // thus, let's add the prefix http://
+                                if (!stristr($this->_hostname,'http')) {
+                                        $url = parse_url('http://'.$this->_hostname);
+                                } else {
+                                        $url = parse_url($this->_hostname);
+                                }
+                                return $url['host'];
+                        }
+                        return $this->_hostname;
 		}
 		
 		public function loadFixtures()
