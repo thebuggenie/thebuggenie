94562a01bb32be5cd8875b3195c9dc9bd43ad5e6
Daniel André Eikeland <zegenie@gmail.com>
2012-11-16 14:04:20 +0100

    fix an issue where an incorrect css or js file could be included

--- modules/main/classes/actions.class.php
+++ modules/main/classes/actions.class.php
@@ -3707,7 +3707,7 @@
 					header('Content-type: text/javascript');
 					foreach($itemarray['js'] as $file)
 					{
-						if(file_exists($file))
+                        if(file_exists($file) && strpos(realpath($file), THEBUGGENIE_PATH) !== false)
 						{
 							echo file_get_contents($file);
 						}
@@ -3718,7 +3718,7 @@
 					header('Content-type: text/css');
 					foreach($itemarray['css'] as $file)
 					{
-						if(file_exists($file))
+                        if(file_exists($file) && strpos(realpath($file), THEBUGGENIE_PATH) !== false)
 						{
 							echo file_get_contents($file);
 						}
