9db8c3102ba9e2fb6e4aae1ab70609537ba99c5f
Daniel André Eikeland <zegenie@gmail.com>
2013-02-21 12:39:09 +0100

    use correct js function to remove issuetype scheme (fixes issue #1835)

--- modules/configuration/templates/_issuetypescheme.inc.php
+++ modules/configuration/templates/_issuetypescheme.inc.php
@@ -49,7 +49,7 @@
 		<div class="header"><?php echo __('Are you sure?'); ?></div>
 		<div class="content">
 			<?php echo __('Please confirm that you want to delete this issue type scheme.'); ?><br>
-			<form accept-charset="<?php echo TBGContext::getI18n()->getCharset(); ?>" action="<?php echo make_url('configure_issuetypes_delete_scheme', array('scheme_id' => $scheme->getID())); ?>" onsubmit="TBG.Config.Issuetype.removeScheme('<?php echo make_url('configure_issuetypes_delete_scheme', array('scheme_id' => $scheme->getID())); ?>', <?php echo $scheme->getID(); ?>);return false;" id="delete_issuetype_scheme_<?php echo $scheme->getID(); ?>_form">
+			<form accept-charset="<?php echo TBGContext::getI18n()->getCharset(); ?>" action="<?php echo make_url('configure_issuetypes_delete_scheme', array('scheme_id' => $scheme->getID())); ?>" onsubmit="TBG.Config.IssuetypeScheme.remove('<?php echo make_url('configure_issuetypes_delete_scheme', array('scheme_id' => $scheme->getID())); ?>', <?php echo $scheme->getID(); ?>);return false;" id="delete_issuetype_scheme_<?php echo $scheme->getID(); ?>_form">
 				<div style="text-align: right;">
 					<?php echo image_tag('spinning_16.gif', array('style' => 'margin-right: 5px; display: none;', 'id' => 'delete_issuetype_scheme_'.$scheme->getID().'_indicator')); ?>
 					<input type="submit" value="<?php echo __('Yes, delete it'); ?>"><?php echo __('%delete% or %cancel%', array('%delete%' => '', '%cancel%' => '<b>'.javascript_link_tag(__('cancel'), array('onclick' => "$('delete_scheme_{$scheme->getID()}_popup').toggle();")).'</b>')); ?>
