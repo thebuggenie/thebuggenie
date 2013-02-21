6925e402975943e4d6d37682200648ad4c62f26c
Daniel André Eikeland <zegenie@gmail.com>
2013-02-21 12:02:59 +0100

    add more permission checks to move issue popup (fixing #1829)

--- modules/main/templates/_moveissue.inc.php
+++ modules/main/templates/_moveissue.inc.php
@@ -1,4 +1,4 @@
-<div class="backdrop_box medium" id="viewissue_add_item_div">
+<div class="backdrop_box medium" id="viewissue_move_issue_div">
 	<div class="backdrop_detail_header"><?php echo __('Move issue to a different project'); ?></div>
 	<div id="backdrop_detail_content" class="backdrop_detail_content">
 		<form action="<?php echo make_url('move_issue', array('issue_id' => $issue->getID())); ?>" method="post">
@@ -8,7 +8,7 @@
 			<label for="move_issue_project"><?php echo __('Move issue to'); ?></label><br>
 			<select name="project_id">
 				<?php foreach (TBGProject::getAll() as $project): ?>
-					<?php if (!$tbg_user->canReportIssues($project) || $project->getID() == $issue->getProject()->getID()) continue; ?>
+					<?php if (!$project->hasAccess() || $project->isDeleted() || $project->isArchived() || !$tbg_user->canReportIssues($project) || $project->getID() == $issue->getProject()->getID()) continue; ?>
 					<option value="<?php echo $project->getID(); ?>"<?php if ($project->getID() == $issue->getProject()->getID()): ?> selected<?php endif; ?>><?php echo $project->getName(); ?></option>
 				<?php endforeach; ?>
 			</select>
@@ -16,11 +16,10 @@
 				<input type="submit" value="<?php echo __('Move issue'); ?>" onclick="$(this).hide();$('move_issue_indicator').show();">
 				<?php echo image_tag('spinning_16.gif', array('id' => 'move_issue_indicator', 'style' => 'display: none; margin-right: 5px;')); ?>
 				<?php echo __('%move_issue% or %cancel%', array('%move_issue%' => '', '%cancel%' => '')); ?>
-				<a href="javascript:void(0)" onclick="$('move_issue').hide();"><?php echo __('cancel'); ?></a>
+				<a href="javascript:void(0)" onclick="TBG.Main.Helpers.Backdrop.reset();"><?php echo __('cancel'); ?></a>
 			</div>
 		</form>
 	</div>
 	<div class="backdrop_detail_footer">
-		<a href="javascript:void(0);" onclick="TBG.Main.Helpers.Backdrop.reset();"><?php echo __('Cancel'); ?></a>
 	</div>
 </div>
