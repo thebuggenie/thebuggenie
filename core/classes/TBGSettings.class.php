306a4ac4cc210dae7a32a5c0d8633c3c6cfcad8f
Daniel André Eikeland <zegenie@gmail.com>
2013-02-28 21:13:59 +0100

    prepare for 3.2.5 tag

--- core/classes/TBGSettings.class.php
+++ core/classes/TBGSettings.class.php
@@ -106,8 +106,8 @@
 
 		protected static $_ver_mj = 3;
 		protected static $_ver_mn = 2;
-		protected static $_ver_rev = '5-dev';
-		protected static $_ver_name = "Aut viam inveniam aut faciam";
+		protected static $_ver_rev = 5;
+		protected static $_ver_name = "De gustibus non est disputandum";
 		protected static $_defaultscope = null;
 		protected static $_settings = null;
 		protected static $_loadedsettings = array();
