c051b6bb307d0c88ac74f155d8c7035be1048471
Richard <Rvanlaak@gmail.com>
2013-09-28 11:13:12 +0200

    * initialize the composer autoloader

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -8,7 +8,7 @@
 	date_default_timezone_set('UTC');
 	
 	if (!defined('THEBUGGENIE_PATH'))
-		throw new Exception('You must define the THEBUGGENIE_PATH constant so we can find the files we need');
+		throw new \Exception('You must define the THEBUGGENIE_PATH constant so we can find the files we need');
 
 	// Load the context class, which controls most of things
 	require THEBUGGENIE_CORE_PATH . 'classes' . DS . 'TBGContext.class.php';
@@ -22,3 +22,9 @@
 	TBGContext::autoloadNamespace('b2db', THEBUGGENIE_CORE_PATH . 'B2DB' . DS);
 
 	TBGContext::initialize();
+
+	// Initialize all composer loaded vendor packages
+	if (! file_exists(THEBUGGENIE_CORE_PATH . 'lib' . DS . 'autoload.php')) {
+		throw new \Exception('You must initialize vendor libraries by running `composer.phar install` via cli');
+	} 
+	require THEBUGGENIE_CORE_PATH . 'lib' . DS . 'autoload.php';
