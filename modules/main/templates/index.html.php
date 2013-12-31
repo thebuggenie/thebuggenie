2dbc10b2a8faf8797ec49c72bb97f96471c203c5
Daniel Andre Eikeland <zegenie@gmail.com>
2013-12-31 09:54:12 +0100

    fix frontpage single tracker info box

--- modules/main/templates/index.html.php
+++ modules/main/templates/index.html.php
@@ -6,7 +6,7 @@
 ?>
 <?php if ($show_project_config_link && $show_project_list): ?>
 	<?php if ($project_count == 1): ?>
-		<?php include_component('main/hideableInfoBoxModal', array('key' => 'index_single_project_mode', 'title' => __('Only using The Bug Genie to track issues for one project?'), 'content' => __("It looks likes you're only using The Bug Genie to track issues for one project. If you don't want to use this homepage, you can set The Bug Genie to <i>single project tracker mode</i>, which will automatically forward the frontpage to the project overview page.<br><br><i>Single project tracker mode</i> can be enabled from %configure_settings%.", array('%configure_settings%' => link_tag(make_url('configure_settings'), '<b>' . __('Configure &ndash;&gt; Settings') . '</b>'))))); ?>
+		<?php include_component('main/hideableInfoBoxModal', array('key' => 'index_single_project_mode', 'title' => __('Only using The Bug Genie to track issues for one project?'), 'template' => 'main/intro_index_single_tracker')); ?>
 	<?php elseif ($project_count == 0): ?>
 		<?php include_component('main/hideableInfoBoxModal', array('key' => 'index_no_projects', 'title' => __('Get started using The Bug Genie'), 'template' => 'main/intro_index_no_projects')); ?>
 	<?php endif; ?>
