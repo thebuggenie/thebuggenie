32266d6305599a98fe379b035104bcb515379441
milkyway-git <ackerm75@hotmail.com>
2014-04-27 18:46:13 +0200

    Adding german plain text template for test mails

---
+++ modules/mailing/i18n/de_DE/templates/_testemail.text.inc.php
@@ -0,0 +1,4 @@
+Hi!
+Dies ist eine Test E-Mail um die korrekte Installation des Moduls E-Mail-Benachrichtigungen auf <?php echo $module->generateURL('home'); ?> sicherzustellen.
+
+Weil Sie diese E-Mail erhalten haben, sollte alles korrekt eingerichtet sein.
