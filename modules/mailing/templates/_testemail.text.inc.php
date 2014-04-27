8496512ce4435dc3ba6bea4243543b53991d231b
milkyway-git <ackerm75@hotmail.com>
2014-04-27 12:30:34 +0200

    Replaced obsolete parameter %thebuggenie_url%

--- modules/mailing/templates/_testemail.text.inc.php
+++ modules/mailing/templates/_testemail.text.inc.php
@@ -1,4 +1,4 @@
 Hi!
-This is a test email checking that the mail notification module at %thebuggenie_url% is correctly set up.
+This is a test email checking that the mail notification module at <?php echo $module->generateURL('home'); ?> is correctly set up.
 
 Since you received this email, everything should be properly set up.
