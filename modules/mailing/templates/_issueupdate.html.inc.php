69e971872958936f3e8e3587335fc30bb96e4587
milkyway-git <ackerm75@hotmail.com>
2014-04-28 19:37:14 +0200

    Translation

--- modules/mailing/templates/_issueupdate.html.inc.php
+++ modules/mailing/templates/_issueupdate.html.inc.php
@@ -1,11 +1,11 @@
 <?php if ($issue instanceof TBGIssue): ?>
 	<h3>
 		<?php echo $issue->getFormattedTitle(true); ?><br>
-		<span style="font-size: 0.8em; font-weight: normal;"><?php echo __('Updated by') . ' ' . $updated_by->getBuddyname() . ' (' . $updated_by->getUsername() . ')'; ?></span><br>
-		<span style="font-size: 0.8em; color: #AAA; font-weight: normal;"><?php echo __('Created by') . ' ' . $issue->getPostedBy()->getBuddyname() . ' (' . $issue->getPostedBy()->getUsername() . ')'; ?></span>
+		<span style="font-size: 0.8em; font-weight: normal;"><?php echo __('Updated by %name', array('%name' => $updated_by->getBuddyname())) . ' (' . $updated_by->getUsername() . ')'; ?></span><br>
+		<span style="font-size: 0.8em; color: #AAA; font-weight: normal;"><?php echo __('Created by %name', array('%name' => $issue->getPostedBy()->getBuddyname())) . ' (' . $issue->getPostedBy()->getUsername() . ')'; ?></span>
 	</h3>
 	<?php if (isset($comment) && $comment instanceof TBGComment): ?>
-		<h4><?php echo __('Comment by') . ' ' . $comment->getPostedBy()->getBuddyname() . ' (' . $comment->getPostedBy()->getUsername() . ')'; ?></h4>
+		<h4><?php echo __('Comment by %name', array('%name' => $comment->getPostedBy()->getBuddyname())) . ' (' . $comment->getPostedBy()->getUsername() . ')'; ?></h4>
 		<p><?php echo $comment->getParsedContent(); ?></p>
 		<br>
 	<?php endif; ?>
@@ -108,7 +108,7 @@
 	<div style="color: #888;">
 		<?php echo __('Show issue:') . ' ' . link_tag($module->generateURL('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo()))); ?><br>
 		<?php if (isset($comment) && $comment instanceof TBGComment){echo __('Show comment:') . ' ' . link_tag($module->generateURL('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo())).'#comment_'.$comment->getID()) . "<br>";} ?>
-		<?php echo __('Show') . ' ' . $issue->getProject()->getName() . ' ' . __('project dashboard:') . ' ' . link_tag($module->generateURL('project_dashboard', array('project_key' => $issue->getProject()->getKey()))); ?><br>
+		<?php echo __('Show %project project dashboard:', array('%project' => $issue->getProject()->getName())) . ' ' . link_tag($module->generateURL('project_dashboard', array('project_key' => $issue->getProject()->getKey()))); ?><br>
 		<br>
 		<?php echo __('You were sent this notification email because you are related to, subscribed to, or commented on the issue mentioned in this email.');?><br>
 		<?php echo __('Depending on your notification settings, you may or may not be notified again when this issue is updated in the future.');?><br>
