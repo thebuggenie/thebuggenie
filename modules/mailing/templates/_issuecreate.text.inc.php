0047a7e774ea98f7db1b138ac41a3ee8b1984ce5
milkyway-git <ackerm75@hotmail.com>
2014-05-01 19:05:59 +0200

    Translation

--- modules/mailing/templates/_issuecreate.text.inc.php
+++ modules/mailing/templates/_issuecreate.text.inc.php
@@ -1,16 +1,20 @@
 * <?php echo $issue->getFormattedTitle(true); ?> *
-Created by <?php echo $issue->getPostedBy()->getBuddyname(); ?> (<?php echo $issue->getPostedBy()->getUsername(); ?>)
+<?php echo __('Created by %name', array('%name' =>  $issue->getPostedBy()->getBuddyname())) . ' (' . $issue->getPostedBy()->getUsername() . ')'; ?>
 
-* Description: *
+
+* <?php echo __('Description') . ':'; ?> *
 <?php echo tbg_parse_text($issue->getDescription()); ?>
 
 <?php if ($issue->getReproductionSteps()): ?>
-	* Reproduction steps: *
+	* <?php echo __('Reproduction steps') . ':'; ?> *
 	<?php echo tbg_parse_text($issue->getReproductionSteps()); ?>
 <?php endif; ?>
 
-Show issue: <?php echo $module->generateURL('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo())); ?>
-Show <?php echo $issue->getProject()->getName(); ?> project dashboard: <?php echo $module->generateURL('project_dashboard', array('project_key' => $issue->getProject()->getKey())); ?>
+<?php echo __('Show issue:') . ' ' . $module->generateURL('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo())); ?>
 
-You were sent this notification email because you are related to the issue mentioned in this email.
-To change when and how often we send these emails, update your account settings: <?php echo link_tag($module->generateURL('account'), $module->generateURL('account')); ?>
+<?php echo __('Show %project project dashboard:', array('%project' => $issue->getProject()->getName())) . ' ' . $module->generateURL('project_dashboard', array('project_key' => $issue->getProject()->getKey())); ?>
+
+
+<?php echo __('You were sent this notification email because you are related to the issue mentioned in this email.'); ?>
+
+<?php echo __('To change when and how often we send these emails, update your account settings:') . ' ' . $module->generateURL('account'); ?>
