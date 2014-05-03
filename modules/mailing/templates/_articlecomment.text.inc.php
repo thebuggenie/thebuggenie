428c484172bf95b7daaf24ea4d1da0a598c8569c
milkyway-git <ackerm75@hotmail.com>
2014-05-03 10:39:54 +0200

    Translation

--- modules/mailing/templates/_articlecomment.text.inc.php
+++ modules/mailing/templates/_articlecomment.text.inc.php
@@ -1,12 +1,14 @@
 * <?php echo $article->getTitle(); ?> *
 
-A comment has been posted:
+<?php echo __('A comment has been posted:'); ?>
 
-* Comment by <?php echo $comment->getPostedBy()->getBuddyname(); ?> (<?php echo $comment->getPostedBy()->getUsername(); ?>) *
+
+* <?php echo __('Comment by %name', array('%name' => $comment->getPostedBy()->getBuddyname())) .' (' . $comment->getPostedBy()->getUsername() . ')'; ?> *
 <?php echo $comment->getContent(); ?>
 
-Show article: <?php echo $module->generateURL('publish_article', array('article_name' => $article->getTitle()))."\n"; ?>
-Show comment: <?php echo $module->generateURL('publish_article', array('article_name' => $article->getTitle())).'#comment_'.$comment->getID()."\n"; ?>
 
-You were sent this notification email because you are related to the article mentioned in this email.
-To change when and how often we send these emails, update your account settings: <?php echo link_tag($module->generateURL('account'), $module->generateURL('account')); ?>
+<?php echo __('Show article:') . ' ' . $module->generateURL('publish_article', array('article_name' => $article->getTitle()))."\n"; ?>
+<?php echo __('Show comment:') . ' ' . $module->generateURL('publish_article', array('article_name' => $article->getTitle())).'#comment_'.$comment->getID()."\n"; ?>
+
+<?php echo __('You were sent this notification email because you are related to the article mentioned in this email.')."\n"; ?>
+<?php echo __('To change when and how often we send these emails, update your account settings:') . ' ' . $module->generateURL('account'); ?>
