b9976fb90325480e87ff2eeb352272d6ef4509e9
milkyway-git <ackerm75@hotmail.com>
2014-05-03 10:40:19 +0200

    Translation

--- modules/mailing/templates/_articlecomment.html.inc.php
+++ modules/mailing/templates/_articlecomment.html.inc.php
@@ -1,14 +1,14 @@
 <?php if ($article instanceof TBGWikiArticle && $comment instanceof TBGComment): ?>
 	<h3><?php echo $article->getTitle(); ?></h3>
 	<br>
-	<h4>Comment by <?php echo $comment->getPostedBy()->getBuddyname(); ?> (<?php echo $comment->getPostedBy()->getUsername(); ?>)</h4>
+	<h4><?php echo __('Comment by %name', array('%name' => $comment->getPostedBy()->getBuddyname())) . ' ( ' . $comment->getPostedBy()->getUsername() . ')'; ?></h4>
 	<p><?php echo $comment->getParsedContent(); ?></p>
 	<br>
 	<div style="color: #888;">
-		Show article: <?php echo link_tag($module->generateURL('publish_article', array('article_name' => $article->getTitle()))); ?><br>
-		Show comment: <?php echo link_tag($module->generateURL('publish_article', array('article_name' => $article->getTitle())).'#comment_'.$comment->getID()); ?><br>
+		<?php echo __('Show article:') . ' ' . link_tag($module->generateURL('publish_article', array('article_name' => $article->getTitle()))); ?><br>
+		<?php echo __('Show comment:') . ' ' . link_tag($module->generateURL('publish_article', array('article_name' => $article->getTitle())).'#comment_'.$comment->getID()); ?><br>
 		<br>
-		You were sent this notification email because you are related to the article mentioned in this email.<br>
-		To change when and how often we send these emails, update your account settings: <?php echo link_tag($module->generateURL('account'), $module->generateURL('account')); ?>
+		<?php echo __('You were sent this notification email because you are related to the article mentioned in this email.')?><br>
+		<?php echo __('To change when and how often we send these emails, update your account settings:') . ' ' . link_tag($module->generateURL('account'), $module->generateURL('account')); ?>
 	</div>
 <?php endif; ?>
