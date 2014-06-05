5edb23335d173b41d06bcefddae1006309806d9c
Daniel Andre Eikeland <zegenie@gmail.com>
2014-06-05 05:46:19 +0200

    stop showing attachments four times

--- modules/main/templates/_issuedetailslisteditable.inc.php
+++ modules/main/templates/_issuedetailslisteditable.inc.php
@@ -659,15 +659,6 @@
 			<?php foreach ($issue->getFiles() as $file_id => $file): ?>
 				<?php include_component('main/attachedfile', array('base_id' => 'viewissue_files', 'mode' => 'issue', 'issue' => $issue, 'file' => $file)); ?>
 			<?php endforeach; ?>
-			<?php foreach ($issue->getFiles() as $file_id => $file): ?>
-				<?php include_component('main/attachedfile', array('base_id' => 'viewissue_files', 'mode' => 'issue', 'issue' => $issue, 'file' => $file)); ?>
-			<?php endforeach; ?>
-			<?php foreach ($issue->getFiles() as $file_id => $file): ?>
-				<?php include_component('main/attachedfile', array('base_id' => 'viewissue_files', 'mode' => 'issue', 'issue' => $issue, 'file' => $file)); ?>
-			<?php endforeach; ?>
-			<?php foreach ($issue->getFiles() as $file_id => $file): ?>
-				<?php include_component('main/attachedfile', array('base_id' => 'viewissue_files', 'mode' => 'issue', 'issue' => $issue, 'file' => $file)); ?>
-			<?php endforeach; ?>
 		</ul>
 	</div>
 </fieldset>
