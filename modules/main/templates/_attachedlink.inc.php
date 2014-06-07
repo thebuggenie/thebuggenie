c4759fdb5c97a74a9822197047e40e55b10af451
milkyway <ackerm75@hotmail.com>
2014-06-07 14:17:18 +0200

    Replaced undefined variables with link_id

--- modules/main/templates/_attachedlink.inc.php
+++ modules/main/templates/_attachedlink.inc.php
@@ -5,7 +5,7 @@
 	</a>
 	<?php if ($issue->canRemoveAttachments()): ?>
 		<div class="removelink">
-			<?php echo javascript_link_tag(image_tag('action_delete.png'), array('id' => $base_id . '_' . $file_id . '_remove_link', 'onclick' => "TBG.Main.Helpers.Dialog.show('".__('Do you really want to remove this link?')."', '".__('This aciton cannot be reversed. Are you sure you want to do this?')."', {yes: {click: function() {TBG.Issues.Link.remove('".make_url('issue_remove_link', array('issue_id' => $issue->getID(), 'link_id' => $link_id))."', ".$link_id."); }}, no: { click: TBG.Main.Helpers.Dialog.dismiss }});")); ?>
+			<?php echo javascript_link_tag(image_tag('action_delete.png'), array('id' => $link_id . '_remove_link', 'onclick' => "TBG.Main.Helpers.Dialog.show('".__('Do you really want to remove this link?')."', '".__('This aciton cannot be reversed. Are you sure you want to do this?')."', {yes: {click: function() {TBG.Issues.Link.remove('".make_url('issue_remove_link', array('issue_id' => $issue->getID(), 'link_id' => $link_id))."', ".$link_id."); }}, no: { click: TBG.Main.Helpers.Dialog.dismiss }});")); ?>
 			<?php echo image_tag('spinning_16.gif', array('id' => 'viewissue_links_' . $link_id . '_remove_indicator', 'style' => 'display: none;')); ?>
 		</div>
 	<?php endif; ?>
