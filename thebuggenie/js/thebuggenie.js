761822726ded8d45d00a900438c60987ed87c997
stolenbike <sven.jakobs@hologic.com>
2014-08-05 14:11:16 -0400

    Fix "Change my password" popup bug
    An uninitialized variable prevented the clearPopupsAndButtons()
    javascript function from finishing.
    As result the action dropdown menu in the security tab of "Your account"
    was not showing up anymore which prevented that users could change their
    password.

--- thebuggenie/js/thebuggenie.js
+++ thebuggenie/js/thebuggenie.js
@@ -1314,9 +1314,11 @@ TBG.Main.Profile.confirmScopeMembership = function(url, sid) {
 TBG.Main.Profile.clearPopupsAndButtons = function(event) {
 	if ($('account_info_container')) {
 		var pbuttons = $('account_info_container').down('.profile_buttons');
-		pbuttons.select('.button').each(function(element) {
-			$(element).removeClassName('button-pressed');
-		});
+		if (typeof pbuttons !== 'undefined'){
+			pbuttons.select('.button').each(function(element) {
+				$(element).removeClassName('button-pressed');
+			});
+		}
 	}
 	$$('.popup_box').each(function(element) {
 		var prev = $(element).previous();
