62b65e0ad5703b5a31d6752c5fc07e7af60a93c4
Daniel André Eikeland <zegenie@gmail.com>
2014-11-11 08:18:28 +0100

    add percentage in milestone status tooltips

--- core/modules/project/templates/_milestonewhiteboardstatusdetails.inc.php
+++ core/modules/project/templates/_milestonewhiteboardstatusdetails.inc.php
@@ -5,7 +5,7 @@
         <div class="statusblock" style="background-color: <?php echo (isset($statuses[$status['id']])) ? $statuses[$status['id']]->getColor() : '#FFF'; ?>; width: <?php echo $status['percent']; ?>%;" title="<?php echo __('%status_name - %percentage (%count of %total)', array('%status_name' => (isset($statuses[$status['id']])) ? $statuses[$status['id']]->getName() : __('Unknown status'), '%percentage' => $status['percent'].'%', '%count' => $status['count'], '%total' => $status_details['total'])); ?>"></div>
     <?php endforeach; ?>
     </div>
-    <div class="milestone_percentage">
+    <div class="milestone_percentage" title="<?php echo __('%percentage % completed', array('%percentage' => $milestone->getPercentComplete())); ?>">
         <div class="filler" id="milestone_<?php echo $milestone->getID(); ?>_percentage_filler" style="width: <?php echo $milestone->getPercentComplete(); ?>%;" title="<?php echo __('%percentage completed', array('%percentage' => $milestone->getPercentComplete().'%')); ?>"></div>
     </div>
 <?php else: ?>
