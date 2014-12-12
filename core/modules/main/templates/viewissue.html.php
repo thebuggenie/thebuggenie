bc5f408f22971047235f8ebcb5b8a297f88fa6ad
Daniel André Eikeland <zegenie@gmail.com>
2014-12-12 08:29:12 +0100

    user pain header area style fix

--- core/modules/main/templates/viewissue.html.php
+++ core/modules/main/templates/viewissue.html.php
@@ -108,7 +108,7 @@
                         </div>
                     </td>
                     <td style="width: 80px;<?php if (!$issue->isUserPainVisible()): ?> display: none;<?php endif; ?>" id="user_pain_additional">
-                        <div class="rounded_box green borderless" title="<?php echo __('This is the user pain value for this issue'); ?>" id="viewissue_triaging" style="margin: 0 5px 0 0; vertical-align: middle; padding: 5px; font-weight: bold; font-size: 13px; text-align: center">
+                        <div title="<?php echo __('This is the user pain value for this issue'); ?>" id="viewissue_triaging">
                             <div class="user_pain" id="issue_user_pain"><?php echo $issue->getUserPain(); ?></div>
                             <div class="user_pain_calculated" id="issue_user_pain_calculated"><?php echo $issue->getUserPainDiffText(); ?></div>
                         </div>
