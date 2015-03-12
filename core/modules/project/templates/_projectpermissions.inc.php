6ea269217e9ba63ab5d48b21e7c3fc39857ecf95
Premke <mihapremke@gmail.com>
2015-03-12 22:12:26 +0100

    Removed globally available roles from edit project permissions.
    This is highly opinioned but this functionality wasn't working properly since IDs of elements (list, edit) used for globally available roles are not unique across projects on Configure project page and it would require extensive modification on nested components to pass project id therefore these elements are removed. Globally available roles already have separated page - Configure roles page which is functional.

--- core/modules/project/templates/_projectpermissions.inc.php
+++ core/modules/project/templates/_projectpermissions.inc.php
@@ -10,13 +10,6 @@
         <div class="content faded_out">
             <p><?php echo __("Roles are applied when assigning users or teams to a project, granting them access to specific parts of the project or giving users access to update and edit information. Updating permissions in this list will add or remove permissions for all users and / or team members with that role, on all assigned projects. Removing a role removes all permissions granted by that role for all users and teams. Read more about roles and permissions in the %online_documentation", array('%online_documentation' => link_tag('http://issues.thebuggenie.com/wiki/TheBugGenie:RolesAndPermissions', '<b>'.__('online documentation').'</b>'))); ?></p>
         </div>
-        <h4><?php echo __('Globally available roles'); ?></h4>
-        <ul id="global_roles_list" class="simple_list" style="width: 788px;">
-            <?php foreach ($roles as $role): ?>
-                <?php include_component('configuration/role', array('role' => $role)); ?>
-            <?php endforeach; ?>
-            <li class="faded_out" id="global_roles_no_roles"<?php if (count($roles)): ?> style="display: none;"<?php endif; ?>><?php echo __('There are no globally available roles'); ?></li>
-        </ul>
         <h4><?php echo __('Project-specific roles'); ?></h4>
         <div class="rounded_box white shadowed" id="new_project<?php echo $project->getID(); ?>_role" style="display: none; position: absolute; right: 0; z-index: 10">
             <form id="new_project<?php echo $project->getID(); ?>_role_form" method="post" action="<?php echo make_url('project_create_role', array('project_key' => $project->getKey())); ?>" onsubmit="TBG.Project.Roles.add('<?php echo make_url('project_create_role', array('project_key' => $project->getKey())); ?>', <?php echo $project->getID(); ?>); return false;" accept-charset="<?php echo \thebuggenie\core\framework\Context::getI18n()->getCharset(); ?>">
