thebuggenie/thebuggenie

commit 71ebc632e4a4533e4a2827a282a9084ea40d911f

Added default Workflow Transition template "No template used - transition happens instantly".

core/entities/WorkflowTransition.php
@@ -86,7 +86,7 @@
 
         public static function getTemplates()
         {
-            $templates = array('main/updateissueproperties' => 'Set issue properties or add comment');
+            $templates = array('' => null, 'main/updateissueproperties' => 'Set issue properties or add comment');
             $event = \thebuggenie\core\framework\Event::createNew('core', 'workflow_templates', null, array(), $templates)->trigger();
             
             return $event->getReturnList();

commit d6990282200ae7eacb3ad576095cf40c919d63b0

Fixed duplicating of default Workflow Template caused by commit 71ebc632e4a4533e4a2827a282a9084ea40d911f.
Key for default Workflow Template is empty string ('') and for Initial Transition is null to not cause confusion.

core/entities/WorkflowTransition.php
@@ -86,7 +86,7 @@
 
         public static function getTemplates()
         {
-            $templates = array('' => null, 'main/updateissueproperties' => 'Set issue properties or add comment');
+            $templates = array('' => 'No template used - transition happens instantly', 'main/updateissueproperties' => 'Set issue properties or add comment');
             $event = \thebuggenie\core\framework\Event::createNew('core', 'workflow_templates', null, array(), $templates)->trigger();
             
             return $event->getReturnList();

core/modules/configuration/templates/_workflowaddtransition.inc.php
@@ -37,7 +37,6 @@
                         <dt><label for="add_transition_step_<?php echo $step->getID(); ?>_template"><?php echo __('Popup template'); ?></label></dt>
                         <dd>
                             <select id="add_transition_step_<?php echo $step->getID(); ?>_template" name="template">
-                                <option value="" selected><?php echo __('No template used - transition happens instantly'); ?></option>
                                 <?php foreach (\thebuggenie\core\entities\WorkflowTransition::getTemplates() as $template_key => $template_name): ?>
                                     <option value="<?php echo $template_key; ?>"><?php echo $template_name; ?></option>
                                 <?php endforeach; ?>

core/modules/configuration/templates/configureworkflowtransition.html.php
@@ -79,7 +79,6 @@
                                         <dt><label for="edit_transition_<?php echo $transition->getID(); ?>_template"><?php echo __('Popup template'); ?></label></dt>
                                         <dd>
                                             <select id="edit_transition_<?php echo $transition->getID(); ?>_template" name="template">
-                                                <option value=""<?php if ($transition->getTemplate() == ''): ?> selected<?php endif; ?>><?php echo __('No template used - transition happens instantly'); ?></option>
                                                 <?php foreach (\thebuggenie\core\entities\WorkflowTransition::getTemplates() as $template_key => $template_name): ?>
                                                     <option value="<?php echo $template_key; ?>"<?php if ($transition->getTemplate() == $template_key): ?> selected<?php endif; ?>><?php echo $template_name; ?></option>
                                                 <?php endforeach; ?>