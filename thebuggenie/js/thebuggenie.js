thebuggenie/thebuggenie

commit b7d14572d4c6758013975ca5c9ff85adcd801d6c

Quick add User no longer opens Add a User Backdrop.
After user is created, request to fetch user is sent and depending on the more available variable Backdrop would be shown. This logic is removed.

thebuggenie/js/thebuggenie.js
@@ -4098,6 +4098,7 @@ TBG.Config.User.add = function (url, callback_function_for_import, form) {
         failure: {
             callback: function (json) {
                 if (json.allow_import) {
+                if (json.allow_import || false) {
                     callback_function_for_import();
                 }
             }

core/modules/configuration/templates/findusers.html.php
@@ -54,11 +54,6 @@
 <?php endif; ?>
 <?php if (isset($more_available)): ?>
     <script type="text/javascript">
-        <?php if (!$more_available): ?>
-            $('adduser_div').hide();
-        <?php else: ?>
-            $('adduser_div').show();
-        <?php endif; ?>
         <?php if (\thebuggenie\core\framework\Context::getScope()->getMaxUsers()): ?>
             $('current_user_num_count').update(<?php echo $total_count; ?>);
         <?php endif; ?>

commit 31557ca6165436327c348bc6068f31ebf141f6e5

Fixed hiding of Add a user Backdrop after successful user creation.
Was kind of broken in my commit b7d14572d4c6758013975ca5c9ff85adcd801d6c.

thebuggenie/js/thebuggenie.js
@@ -4092,9 +4092,9 @@ TBG.Config.User.add = function (url, callback_function_for_import, form) {
         success: {
             update: 'users_results',
             callback: function (json) {
+                $('adduser_div').hide();
                 TBG.Config.User._updateLinks(json);
                 f.reset();
-                $('adduser_div').hide();
             }
         },
         failure: {