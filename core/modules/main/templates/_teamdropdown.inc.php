3534476c640e6ca94834dc4080f2a7c415abe62d
Premke <mihapremke@gmail.com>
2015-04-30 00:53:56 +0200

    Fixed opening and closing of teamdropdown.

--- core/modules/main/templates/_teamdropdown.inc.php
+++ core/modules/main/templates/_teamdropdown.inc.php
@@ -6,7 +6,7 @@
         <?php echo image_tag('icon_team.png', array('class' => "avatar small")); ?>
         <?php echo $team->getName(); ?>
     </a>
-    <div id="team_<?php echo $team->getID() . '_' . $rnd_no; ?>" style="display: none;" class="rounded_box white shadowed user_popup dropdown_box leftie">
+    <div id="team_<?php echo $team->getID() . '_' . $rnd_no; ?>" style="display: none;" class="rounded_box white shadowed user_popup popup_box dropdown_box leftie">
         <div style="padding: 3px;">
             <div style="padding: 2px; width: 36px; height: 36px; text-align: center; background-color: #FFF; border: 1px solid #DDD; float: left;">
                 <?php echo image_tag('team_large.png', array('alt' => ' ', 'style' => "width: 36px; height: 36px;")); ?>
@@ -24,7 +24,7 @@
                 <?php \thebuggenie\core\framework\Event::createNew('core', 'teamactions_bottom', $team)->trigger(); ?>
             </div>
         </div>
-        <div style="text-align: right; padding: 3px; font-size: 9px;"><a href="javascript:void(0);" onclick="$('team_<?php echo $team->getID() . "_" . $rnd_no; ?>').toggle();"><?php echo __('Close this menu'); ?></a></div>
+        <div style="text-align: right; padding: 3px; font-size: 9px;"><a href="javascript:void(0);" onclick="$('team_<?php echo $team->getID() . "_" . $rnd_no; ?>').toggle();$('team_<?php echo $team->getID() . "_" . $rnd_no; ?>').previous().toggleClassName('button-pressed')"><?php echo __('Close this menu'); ?></a></div>
     </div>
 </div>
 <?php endif; ?>
