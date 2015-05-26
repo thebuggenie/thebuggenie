837e8c41257d205014bb5b5016b30a99200c1465
Premke <mihapremke@gmail.com>
2015-05-26 20:50:03 +0200

    Clear saved search entity user if "is_public" flag is true.

--- core/modules/search/Actions.php
+++ core/modules/search/Actions.php
@@ -137,7 +137,7 @@
                 $this->search_object->setName($request['name']);
                 $this->search_object->setDescription($request['description']);
                 $this->search_object->setIsPublic((bool) $request['is_public']);
-                $this->search_object->setUser($this->getUser());
+                $this->search_object->setUser((bool) $request['is_public'] ? 0 : $this->getUser());
                 $this->search_object->setValuesFromRequest($request);
                 if ($request['project_id'])
                     $this->search_object->setAppliesToProject((int) $request['project_id']);
