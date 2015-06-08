2f533633e9a4b1388f89601c72e319fb000e6d1c
Premke <mihapremke@gmail.com>
2015-06-08 22:01:41 +0200

    Cleaning code no longer needed after commit #e208a8e.

--- core/modules/search/Actions.php
+++ core/modules/search/Actions.php
@@ -193,15 +193,6 @@
                 {
                     return $this->redirect('quicksearch');
                 }
-                else
-                {
-                    $issues = $this->issues;
-                    $issue = array_shift($issues);
-                    if ($issue instanceof entities\Issue)
-                    {
-                        return $this->forward($this->getRouting()->generate('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo())));
-                    }
-                }
             }
             if ($this->search_object->hasQuickfoundIssues()) {
                 $issues = $this->search_object->getQuickfoundIssues();
