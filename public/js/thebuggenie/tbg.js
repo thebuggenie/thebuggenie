1bb434b54cb84142a74920e1d136a27d551a11da
Premke <mihapremke@gmail.com>
2015-06-08 19:32:15 +0200

    Change ajax loading indicator for removing issue attachements.

--- public/js/thebuggenie/tbg.js
+++ public/js/thebuggenie/tbg.js
@@ -302,10 +302,10 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'jquery-u
             }
         };
 
-        TBG.Core._detachFile = function (url, file_id, base_id) {
+        TBG.Core._detachFile = function (url, file_id, base_id, loading_indicator) {
             TBG.Main.Helpers.ajax(url, {
                 loading: {
-                    indicator: base_id + file_id + '_remove_indicator',
+                    indicator: typeof(loading_indicator) != 'undefined' ? loading_indicator : base_id + file_id + '_remove_indicator',
                     hide: [base_id + file_id + '_remove_link', 'uploaded_files_' + file_id + '_remove_link'],
                     show: 'uploaded_files_' + file_id + '_remove_indicator'
                 },
@@ -5054,7 +5054,7 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'jquery-u
         }
 
         TBG.Issues.File.remove = function (url, file_id) {
-            TBG.Core._detachFile(url, file_id, 'viewissue_files_');
+            TBG.Core._detachFile(url, file_id, 'viewissue_files_', 'dialog_indicator');
         }
 
         TBG.Issues.Field.setPercent = function (url, mode) {
