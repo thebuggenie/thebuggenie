dc143af329aadffe0654ed128d1a4e1d96db2632
Daniel Andre Eikeland <daniel.andre.eikeland@pearson.com>
2015-06-15 14:48:10 +0200

    fix reference to incorrect action class, breaking mentions

--- core/helpers/TextParserMarkdown.php
+++ core/helpers/TextParserMarkdown.php
@@ -60,7 +60,7 @@
             $user = \thebuggenie\core\entities\tables\Users::getTable()->getByUsername($matches[1]);
             if ($user instanceof \thebuggenie\core\entities\User)
             {
-                $output = \thebuggenie\core\entities\Action::returnComponentHTML('main/userdropdown', array('user' => $matches[1], 'displayname' => $matches[0]));
+                $output = framework\Action::returnComponentHTML('main/userdropdown', array('user' => $matches[1], 'displayname' => $matches[0]));
                 $this->mentions[$user->getID()] = $user;
             }
             else
