5fc3a79d58761e50932322d4707f66a33b376bf3
Premke <mihapremke@gmail.com>
2015-06-17 22:53:28 +0200

    Fixed exception when variable milestone is not set.

--- modules/agile/templates/_whiteboardcontent.inc.php
+++ modules/agile/templates/_whiteboardcontent.inc.php
@@ -1,3 +1,5 @@
-<?php foreach ($board->getMilestoneSwimlanes($milestone) as $swimlane): ?>
-    <?php include_component('agile/boardswimlane', compact('swimlane')); ?>
-<?php endforeach; ?>
+<?php if (isset($milestone)): ?>
+  <?php foreach ($board->getMilestoneSwimlanes($milestone) as $swimlane): ?>
+      <?php include_component('agile/boardswimlane', compact('swimlane')); ?>
+  <?php endforeach; ?>
+<?php endif; ?>
