db9eb84e4690a311478873243e6c58736122e23f
Daniel Andre Eikeland <daniel.andre.eikeland@pearson.com>
2015-06-17 21:13:39 +0200

    Remove untranslated templates for french issue updates

--- modules/mailing/i18n/fr_FR/templates/_issueupdate.text.inc.php
+++
@@ -1,8 +0,0 @@
-Bonjour %user_buddyname%,
-<?php echo $issue->getIssuetype()->getName(); ?> <?php echo $issue->getFormattedTitle(true); ?> a &eacute;t&eacute; mise &agrave; jour par <?php echo $updated_by->getName(); ?>.
-
-<?php echo $comment; ?>
-
----
-Afficher la demande : <?php echo make_url('viewissue', array('project_key' => $issue->getProject()->getKey(), 'issue_no' => $issue->getFormattedIssueNo()), false); ?>
-Affiche le tableau de bord du projet <?php echo $issue->getProject()->getName(); ?> : <?php echo make_url('project_dashboard', array('project_key' => $issue->getProject()->getKey()), false); ?>
