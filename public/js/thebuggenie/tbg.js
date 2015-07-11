57ff97fed714a244c5f9ac98d48b25d4489935c0
Premke <mihapremke@gmail.com>
2015-07-11 14:09:32 +0200

    Reset whiteboard issue column if there is no valid transition to new column.

--- public/js/thebuggenie/tbg.js
+++ public/js/thebuggenie/tbg.js
@@ -2514,6 +2514,8 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'jquery-u
         };
 
         TBG.Project.Planning.Whiteboard.moveIssueColumn = function(issue, column, transition_id) {
+            original_column = issue.parents('.column');
+
             if (issue) {
                 issue.detach().css({left: '0', top: '0'}).prependTo(column);
             }
@@ -2556,6 +2558,11 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'jquery-u
                 },
                 failure: {
                     show: issue
+                    callback: function(json) {
+                        if (json.error != undefined && typeof(json.error) == 'string' && json.error.length) {
+                            issue.detach().css({left: '0', top: '0'}).prependTo(original_column);
+                        }
+                    }
                 }
             });
 
