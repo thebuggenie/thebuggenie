9a8f7edf01c48cedf01166d2f14244aaff91897d
Premke <mihapremke@gmail.com>
2015-07-18 18:45:20 +0200

    Added classes for styling wiki find articles page.

--- modules/publish/templates/findarticles.html.php
+++ modules/publish/templates/findarticles.html.php
@@ -25,13 +25,13 @@
             <h2><?php echo __('Find articles'); ?></h2>
             <form action="<?php echo (\thebuggenie\core\framework\Context::isProjectContext()) ? make_url('publish_find_project_articles', array('project_key' => \thebuggenie\core\framework\Context::getCurrentProject()->getKey())) : make_url('publish_find_articles'); ?>" method="get" accept-charset="<?php echo \thebuggenie\core\framework\Context::getI18n()->getCharset(); ?>">
                 <label for="find_article_name" style="font-size: 1.1em; float: left;"><?php echo __('Find articles by name'); ?></label><input type="text" name="articlename" id="find_article_name" value="<?php echo $articlename; ?>" style="width: 400px; float: left; margin-left: 5px; padding: 2px; font-size: 1.3em;">
-                <input type="submit" value="<?php echo __('Find'); ?>" style="float: left; margin-left: 5px; font-size: 1.2em; padding: 3px 5px !important;">
+                <input type="submit" value="<?php echo __('Find'); ?>" class="wiki-find-articles-button" style="float: left; margin-left: 5px; font-size: 1.2em; padding: 3px 5px !important">
             </form>
             <br style="clear: both;">
             <?php if (isset($resultcount)): ?>
                 <?php if ($resultcount): ?>
                     <div class="header_div" style="font-size: 1.3em;"><?php echo __('Found %num article(s)', array('%num' => $resultcount)); ?></div>
-                    <ul class="simple_list">
+                    <ul class="simple_list wiki-find-articles-list">
                         <?php foreach ($articles as $article): ?>
                         <li style="margin-bottom: 0;">
                             <?php echo link_tag(make_url('publish_article', array('article_name' => $article->getName())), $article->getName(), array('style' => 'font-size: 1.1em;')); ?><br>
