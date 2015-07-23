thebuggenie/thebuggenie

commit dca4f3664b6f93a36ce12e3a585899604c1a5184

Allow more characters than alphabet in article title.

modules/publish/entities/Article.php
@@ -844,7 +844,7 @@
          */
         public function setName($name)
         {
-            $this->_name = preg_replace("/[^A-Za-z0-9 :]/", '', $name);
+            $this->_name = preg_replace('/[^\p{L}\p{N}]/u', '', $name);
         }
 
         /**

commit 49790c4e484d1f4ecbdb58e1e77bb39ef8aca578

Allow ":" (colons) character in wiki article title.
Was broken by commit dca4f3664b6f93a36ce12e3a585899604c1a5184..

Fixes issue #2600

modules/publish/entities/Article.php
@@ -844,7 +844,7 @@
          */
         public function setName($name)
         {
-            $this->_name = preg_replace('/[^\p{L}\p{N}]/u', '', $name);
+            $this->_name = preg_replace('/[^\p{L}\p{N} :]/u', '', $name);
         }
 
         /**

modules/publish/templates/_header.inc.php
@@ -88,7 +88,7 @@
         <?php else: ?>
             <?php if (\thebuggenie\core\framework\Context::isProjectContext()): ?>
                 <?php if (mb_strpos($article_name, ucfirst(\thebuggenie\core\framework\Context::getCurrentProject()->getName())) == 0): ?>
-                    <?php $project_article_name = mb_substr($article_name, mb_strlen(preg_replace('/[^\p{L}\p{N}]/u', '', \thebuggenie\core\framework\Context::getCurrentProject()->getName()))); ?>
+                    <?php $project_article_name = mb_substr($article_name, mb_strlen(preg_replace('/[^\p{L}\p{N} :]/u', '', \thebuggenie\core\framework\Context::getCurrentProject()->getName()))+1); ?>
                     <?php if ($article->getID() && $article->isCategory()): ?><span class="faded_out blue">Category:</span><?php endif; ?><span class="faded_out dark"><?php echo ucfirst(\thebuggenie\core\framework\Context::getCurrentProject()->getName()); ?>:</span><?php echo get_spaced_name($project_article_name); ?>
                 <?php endif; ?>
             <?php elseif (mb_substr($article_name, 0, 9) == 'Category:'): ?>