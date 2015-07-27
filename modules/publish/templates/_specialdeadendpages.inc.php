4383cc7a95d58a3687a92af6750740ab5aab1709
Premke <mihapremke@gmail.com>
2015-07-27 12:26:37 +0200

    Opinionated: changed margin to "15px 0" for ".greybox" on publish module special templates.
    Includes revert of commit 73220ff6f5b26e8d2f6f0d1ba8c2a4ec9608827b.

--- modules/publish/templates/_specialdeadendpages.inc.php
+++ modules/publish/templates/_specialdeadendpages.inc.php
@@ -1,7 +1,7 @@
 <div class="article">
     <div class="header">Special:<?php echo ($projectnamespace != '') ? "<span class='faded_out'>{$projectnamespace}</span>" : ''; ?>Dead End Pages</div>
     <?php if (\thebuggenie\core\framework\Context::isProjectContext()): ?>
-        <div class="greybox" style="margin: 15px;">
+        <div class="greybox" style="margin: 15px 0;">
             <?php echo __('Note: This page lists all dead end articles for "%project_name". For a list of global articles with no links, see %dead_end_pages', array('%project_name' => \thebuggenie\core\framework\Context::getCurrentProject()->getName(), '%dead_end_pages' => link_tag(make_url('publish_article', array('article_name' => "Special:DeadEndPages")), 'Special:DeadEndPages'))); ?>
         </div>
     <?php endif; ?>
