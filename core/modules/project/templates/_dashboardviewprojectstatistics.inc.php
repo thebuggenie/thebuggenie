7580458c7b359224fe83d9c30c8fa154fac8069a
Premke <mihapremke@gmail.com>
2015-10-03 14:59:50 +0200

    Fixed for percent to be filled with greater percentage for dashboard views "Statistics by".

--- core/modules/project/templates/_dashboardviewprojectstatistics.inc.php
+++ core/modules/project/templates/_dashboardviewprojectstatistics.inc.php
@@ -7,13 +7,13 @@
         <tr class="hover_highlight">
             <td><?php echo (is_object($item)) ? $item->getName() : $item; ?></td>
             <td title="<?php echo __('%count issues open of %total_count issues total', array('%count' => $counts[$item_id]['open'], '%total_count' => $counts[$item_id]['open'] + $counts[$item_id]['closed'])); ?>"><?php echo $counts[$item_id]['open']; ?>&nbsp;<span style="font-weight: normal;">/&nbsp;<?php echo $counts[$item_id]['open'] + $counts[$item_id]['closed']; ?> (<?php echo (int) $counts[$item_id]['percentage']; ?>%)</span></td>
-            <td><?php include_component('main/percentbar', array('percent' => 100 - $counts[$item_id]['percentage'], 'height' => 14)); ?></td>
+            <td><?php include_component('main/percentbar', array('percent' => $counts[$item_id]['percentage'], 'height' => 14)); ?></td>
         </tr>
     <?php endforeach; ?>
     <tr class="hover_highlight">
         <td class="faded_out"><?php echo __('Not set'); ?></td>
         <td class="faded_out" title="<?php echo __('%count issues open of %total_count issues total', array('%count' => $counts[0]['open'], '%total_count' => $counts[0]['open'] + $counts[0]['closed'])); ?>"><?php echo $counts[0]['open']; ?>&nbsp;<span style="font-weight: normal;">/&nbsp;<?php echo $counts[0]['open'] + $counts[0]['closed']; ?> (<?php echo (int) $counts[0]['percentage']; ?>%)</span></td>
-        <td class="faded_out"><?php include_component('main/percentbar', array('percent' => 100 - $counts[0]['percentage'], 'height' => 14)); ?></td>
+        <td class="faded_out"><?php include_component('main/percentbar', array('percent' => $counts[0]['percentage'], 'height' => 14)); ?></td>
     </tr>
 </table>
 <?php echo link_tag(make_url('project_statistics', array('project_key' => \thebuggenie\core\framework\Context::getCurrentProject()->getKey())), __('Show more statistics'), array('class' => 'button button-silver dash', 'title' => __('More statistics'))); ?>
