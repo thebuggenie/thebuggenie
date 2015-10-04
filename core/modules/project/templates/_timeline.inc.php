b2f092609e6e990a4e5a8ab6d807af1bd0b66c99
Marko Krečković <m.kreckovic@gmail.com>
2015-10-04 18:55:49 +0200

    Small style fix.

--- core/modules/project/templates/_timeline.inc.php
+++ core/modules/project/templates/_timeline.inc.php
@@ -11,28 +11,28 @@
         <?php foreach ($activities as $activity): ?>
             <?php if ($activity['change_type'] == 'build_release'): ?>
                 <tr>
-                    <td class="imgtd"><?php echo image_tag('icon_build.png'); ?></td>
+                    <td class="imgtd" style="position: absolute; margin-top: -1px; margin-left: 4px;"><?php echo image_tag('icon_build.png'); ?></td>
                     <td style="clear: both;">
                         <span class="time"><?php echo tbg_formatTime($timestamp, 19); ?></span>&nbsp;<span style="font-size: 1.1em"><?php echo $activity['info']; ?></span><br><span style="display: inline-block; margin-top: 4px; margin-bottom: 15px;"><i><?php echo __('New version released'); ?></i></span>
                     </td>
                 </tr>
             <?php elseif ($activity['change_type'] == 'sprint_start'): ?>
                 <tr>
-                    <td class="imgtd"><?php echo image_tag('icon_sprint.png'); ?></td>
+                    <td class="imgtd" style="position: absolute; margin-top: -1px; margin-left: 4px;"><?php echo image_tag('icon_sprint.png'); ?></td>
                     <td style="clear: both;">
                         <span class="time"><?php echo tbg_formatTime($timestamp, 19); ?></span>&nbsp;<span style="font-size: 1.1em"><?php echo $activity['info']; ?></span><br><span style="display: inline-block; margin-top: 4px; margin-bottom: 15px;"><i><?php echo __('A new sprint has started'); ?></i></span>
                     </td>
                 </tr>
             <?php elseif ($activity['change_type'] == 'sprint_end'): ?>
                 <tr>
-                    <td class="imgtd"><?php echo image_tag('icon_sprint.png'); ?></td>
+                    <td class="imgtd" style="position: absolute; margin-top: -1px; margin-left: 4px;"><?php echo image_tag('icon_sprint.png'); ?></td>
                     <td style="clear: both;">
                         <span class="time"><?php echo tbg_formatTime($timestamp, 19); ?></span>&nbsp;<span style="font-size: 1.1em"><?php echo $activity['info']; ?></span><br><span style="display: inline-block; margin-top: 4px; margin-bottom: 15px;"><i><?php echo __('The sprint has ended'); ?></i></span>
                     </td>
                 </tr>
             <?php elseif ($activity['change_type'] == 'milestone_release'): ?>
                 <tr>
-                    <td class="imgtd"><?php echo image_tag('icon_milestone.png'); ?></td>
+                    <td class="imgtd" style="position: absolute; margin-top: -1px; margin-left: 4px;"><?php echo image_tag('icon_milestone.png'); ?></td>
                     <td style="clear: both;">
                         <span class="time"><?php echo tbg_formatTime($timestamp, 19); ?></span>&nbsp;<span style="font-size: 1.1em"><?php echo $activity['info']; ?></span><br><span style="display: inline-block; margin-top: 4px; margin-bottom: 15px;"><i><?php echo __('A new milestone has been reached'); ?></i></span>
                     </td>
