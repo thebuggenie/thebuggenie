a99d807cf85a9d389d162d6539baac6351a97052
Premke <mihapremke@gmail.com>
2015-10-06 18:17:31 +0200

    Fixed undefined index bug when checking transition validation rule for belonging to team.
    When this rule has default value (all teams) variable "$team_id" is empty string.

--- core/entities/WorkflowTransitionValidationRule.php
+++ core/entities/WorkflowTransitionValidationRule.php
@@ -375,6 +375,8 @@
                     }
                     if ($assignee instanceof \thebuggenie\core\entities\User)
                     {
+                        if (count($valid_items) == 1 && reset($valid_items) == '') return true;
+
                         foreach ($valid_items as $team_id)
                         {
                             if ($assignee->isMemberOfTeam($teams[$team_id]))
