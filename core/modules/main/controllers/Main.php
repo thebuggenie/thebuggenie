d427fac75ec4d9a4b1853b1c7ed97d3f994873ff
Premke <mihapremke@gmail.com>
2015-10-15 15:00:31 +0200

    Reworked user notifications settings.
    The settings are now splitted in subscriptions and notifications. You can receive notifications based on system actions or your subscriptions. By default notifications can be receieved in notifications box (visible next to your avatar in top menu) and also via email using Email communication module. Added category select to narrow issues for which to automatically subscribe/get notified.
    
    The flaw with previous settings was too much spamming of users due to coupling of subscribers and notifications.

--- core/modules/main/controllers/Main.php
+++ core/modules/main/controllers/Main.php
@@ -136,6 +136,8 @@ class Main extends framework\Action
 
             $this->getUser()->markNotificationsRead('issue', $issue->getID());
 
+            framework\Context::getUser()->setNotificationSetting(framework\Settings::SETTINGS_USER_NOTIFY_ITEM_ONCE . '_issue_' . $issue->getID(), false)->save();
+
             \thebuggenie\core\framework\Event::createNew('core', 'viewissue', $issue)->trigger();
         }
 
@@ -1165,12 +1167,9 @@ class Main extends framework\Action
     public function runMyAccount(framework\Request $request)
     {
         $this->forward403unless($this->getUser()->hasPageAccess('account'));
-        $notificationsettings = array();
-        $i18n = $this->getI18n();
-        $notificationsettings[framework\Settings::SETTINGS_USER_SUBSCRIBE_CREATED_UPDATED_COMMENTED_ISSUES] = $i18n->__('Automatically subscribe to issues I get involved in');
-        $notificationsettings[framework\Settings::SETTINGS_USER_SUBSCRIBE_CREATED_UPDATED_COMMENTED_ARTICLES] = $i18n->__('Automatically subscribe to article I get involved in');
-        $notificationsettings[framework\Settings::SETTINGS_USER_SUBSCRIBE_NEW_ISSUES_MY_PROJECTS] = $i18n->__('Automatically subscribe to new issues that are created in my project(s)');
-        $notificationsettings[framework\Settings::SETTINGS_USER_SUBSCRIBE_NEW_ARTICLES_MY_PROJECTS] = $i18n->__('Automatically subscribe to new articles that are created in my project(s)');
+        $subscriptionssettings = framework\Settings::getSubscriptionsSettings();
+        $this->subscriptionssettings = $subscriptionssettings;
+        $notificationsettings = framework\Settings:: getNotificationSettings();
         $this->notificationsettings = $notificationsettings;
         $this->has_autopassword = framework\Context::hasMessage('auto_password');
         if ($this->has_autopassword)
@@ -1213,11 +1212,36 @@ class Main extends framework\Action
                     $this->getUser()->setPreferredIssuesSyntax($request['syntax_issues']);
                     $this->getUser()->setPreferredCommentsSyntax($request['syntax_comments']);
                     $this->getUser()->setKeyboardNavigationEnabled($request['enable_keyboard_navigation']);
+                    foreach ($subscriptionssettings as $setting => $description)
+                    {
+                        if ($request->hasParameter('core_' . $setting))
+                        {
+                            if ($setting == framework\Settings::SETTINGS_USER_SUBSCRIBE_NEW_ISSUES_MY_PROJECTS_CATEGORY)
+                            {
+                                $this->getUser()->setNotificationSetting($setting, $request->getParameter('core_' . $setting))->save();
+                            }
+                            else
+                            {
+                                $this->getUser()->setNotificationSetting($setting, true)->save();
+                            }
+                        }
+                        else
+                        {
+                            $this->getUser()->setNotificationSetting($setting, false)->save();
+                        }
+                    }
                     foreach ($notificationsettings as $setting => $description)
                     {
                         if ($request->hasParameter('core_' . $setting))
                         {
-                            $this->getUser()->setNotificationSetting($setting, true)->save();
+                            if ($setting == framework\Settings::SETTINGS_USER_NOTIFY_NEW_ISSUES_MY_PROJECTS)
+                            {
+                                $this->getUser()->setNotificationSetting($setting, $request->getParameter('core_' . $setting))->save();
+                            }
+                            else
+                            {
+                                $this->getUser()->setNotificationSetting($setting, true)->save();
+                            }
                         }
                         else
                         {
@@ -3589,7 +3613,7 @@ class Main extends framework\Action
                     break;
                 case 'reportissue':
                     $this->_loadSelectedProjectAndIssueTypeFromRequestForReportIssueAction($request);
-                    if ($this->selected_project instanceof entities\Project && !$this->selected_project->isLocked() && $tbg_user->canReportIssues($this->selected_project)) {
+                    if ($this->selected_project instanceof entities\Project && !$this->selected_project->isLocked() && $this->getUser()->canReportIssues($this->selected_project)) {
                         $template_name = 'main/reportissuecontainer';
                         $options['selected_project'] = $this->selected_project;
                         $options['selected_issuetype'] = $this->selected_issuetype;
