4d8c0a552f6f9ffd406040b43bca5c24b1c963d9
Premke <mihapremke@gmail.com>
2015-10-22 21:41:51 +0200

    Fixed mentions list for existing comments.
    Variable "$mentionable_target_type" wasn't passed for existing comments and thus all mentionable users for issue / article weren't returned.

--- core/modules/main/templates/_comments.inc.php
+++ core/modules/main/templates/_comments.inc.php
@@ -37,7 +37,7 @@
     <?php foreach (\thebuggenie\core\entities\Comment::getComments($target_id, $target_type, \b2db\Criteria::SORT_DESC) as $comment): ?>
         <?php
 
-            $options = compact('comment', 'comment_count_div');
+            $options = compact('comment', 'comment_count_div', 'mentionable_target_type');
             if (isset($issue))
                 $options['issue'] = $issue;
 
