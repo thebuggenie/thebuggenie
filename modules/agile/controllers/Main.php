e2e6b45287a0a42daff9e38b3c057ba0f502126d
Premke <mihapremke@gmail.com>
2015-11-05 23:01:36 +0100

    Fixed invalid check.

--- modules/agile/controllers/Main.php
+++ modules/agile/controllers/Main.php
@@ -366,7 +366,7 @@
             $board = entities\tables\AgileBoards::getTable()->selectById($request['board_id']);
             $issue = \thebuggenie\core\entities\Issue::getB2DBTable()->selectById($request['issue_id']);
 
-            if ($issue instanceof \thebuggenie\core\entities\Issue && $issue->hasAccess()) return $this->renderJSON(array('child_issue' => 0, 'issue_details' => array(), 'deleted' => 1));
+            if ($issue instanceof \thebuggenie\core\entities\Issue && !$issue->hasAccess()) return $this->renderJSON(array('child_issue' => 0, 'issue_details' => array(), 'deleted' => 1));
 
             $text = array('child_issue' => 0, 'issue_details' => $issue->toJSON(), 'deleted' => $issue->isDeleted() ? 1 : 0);
 
