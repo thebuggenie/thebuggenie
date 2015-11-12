58ce2412af2b136c8067b20f49adf4bc849b9b92
Premke <mihapremke@gmail.com>
2015-11-12 09:55:38 +0100

    Removed custom function "dd" used for quick local debugging.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,5 @@
 <?php
 
-
-
-    function dd()
-    {
-        array_map(function($x) { var_dump($x); }, func_get_args()); die;
-    }
-
     // Set the path to The Bug Genie top folder
     $path = realpath(getcwd());
     defined('DS') || define('DS', DIRECTORY_SEPARATOR);
