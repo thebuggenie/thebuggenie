thebuggenie/thebuggenie

commit dba6cc7bd5e6a1e1cf4fb1582c3820da660fe890

Added toggle of notifications without showing box on TBG.initialize.
Reason is that this way desktop notifications will be shown without user having to click to "subscribe" to new notifications.

public/js/thebuggenie/tbg.js
@@ -439,6 +439,7 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'TweenMax
             document.observe('keydown', TBG.Core._escapeWatcher);
 
             TBG.Core.Pollers.Callbacks.dataPoller();
+            TBG.Main.Profile.toggleNotifications(false);
             TBG.OpenID.init();
             // Mimick browser scroll to element with id as hash once header get 'fixed' class
             // from _scrollWatcher method.
@@ -502,15 +503,16 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'TweenMax
             }
         };
 
-        TBG.Main.Profile.toggleNotifications = function () {
+        TBG.Main.Profile.toggleNotifications = function (toggle_classes) {
             var un = $('user_notifications');
             var unc = $('user_notifications_container');
-            unc.toggleClassName('active');
+            if (toggle_classes == null) toggle_classes = true;
+            if (toggle_classes) unc.toggleClassName('active');
             if (un.hasClassName('active')) {
                 un.removeClassName('active');
             } else {
                 un.style.right = (jQuery(window).width() - (jQuery('#user_notifications_container').offset().left + jQuery('#user_notifications_container').outerWidth()) - parseInt(jQuery('#user_notifications').css('border-right-width'), 10)) + 'px';
-                un.addClassName('active');
+                if (toggle_classes) un.addClassName('active');
                 if ($('user_notifications_list').childElements().size() == 0) {
                     TBG.Main.Helpers.ajax($('user_notifications_list').dataset.notificationsUrl, {
                         url_method: 'get',

commit f56e276f942497cc6336f4b0a4d6e3d8acfcab24

Fixed bug with toggling notifications without box.
Bug was introduced in commit dba6cc7bd5e6a1e1cf4fb1582c3820da660fe890. Element "#user_notifications_container" does not exist for guest session.

public/js/thebuggenie/tbg.js
@@ -506,6 +506,7 @@ define(['prototype', 'effects', 'controls', 'scriptaculous', 'jquery', 'TweenMax
         TBG.Main.Profile.toggleNotifications = function (toggle_classes) {
             var un = $('user_notifications');
             var unc = $('user_notifications_container');
+            if (! un || ! unc) return false;
             if (toggle_classes == null) toggle_classes = true;
             if (toggle_classes) unc.toggleClassName('active');
             if (un.hasClassName('active')) {