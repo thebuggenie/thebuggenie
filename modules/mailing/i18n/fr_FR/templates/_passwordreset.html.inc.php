0d67fca06d6080272062a627a99d5b9245a7257f
milkyway <ackerm75@hotmail.com>
2016-12-20 14:16:04 +0100

    Removed obsolete translation template

--- modules/mailing/i18n/fr_FR/templates/_passwordreset.html.inc.php
+++
@@ -1,7 +0,0 @@
-<div style="font-family: 'Trebuchet MS', 'Liberation Sans', 'Bitstream Vera Sans', 'Luxi Sans', Verdana, sans-serif; font-size: 11px; color: #646464;">
-    <b>Votre nouveau mot de passe a &eacute;t&eacute; enregistr&eacute;</b><br>
-    Bonjour %user_buddyname%,<br>
-    Une demande a &eacute;t&eacute; faite pour la reg&eacute;n&eacute;ration de votre mot de passe pour votre compte &agrave; <a href="%thebuggenie_url%">%thebuggenie_url%</a>.<br>
-    Le nouveau mot de passe a &eacute;t&eacute; enregistr&eacute;. Cliquez sur ce lien : <a href="%thebuggenie_url%">%thebuggenie_url%</a><br>
-    et connectez-vous avec le nom d'utilisateur <b>%user_username%</b> et le mot de passe <b>%password%</b>
-</div>
