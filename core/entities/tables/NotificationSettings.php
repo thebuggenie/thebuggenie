99666b6d5cf5fa0ae2864199052353956bbbcb0e
Daniel AndrÃ© Eikeland <zegenie@gmail.com>
2017-01-05 15:42:44 +0100

    fix notification settings not properly populating (fixes isue #2745)

--- core/entities/tables/NotificationSettings.php
+++ core/entities/tables/NotificationSettings.php
@@ -31,11 +31,11 @@
         public function getByModuleAndNameAndUserId($module, $name, $user_id)
         {
             $crit = $this->getCriteria();
-            $crit->addWhere('notificationsettings.module', $module);
+            $crit->addWhere('notificationsettings.module_name', $module);
             $crit->addWhere('notificationsettings.name', $name);
             $crit->addWhere('notificationsettings.user_id', $user_id);
 
-            return $this->selectOne($this->getCriteria());
+            return $this->selectOne($crit);
         }
 
     }
