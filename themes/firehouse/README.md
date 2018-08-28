16531a0a622f6e898084626b6f63f1243570c374
Daniel André Eikeland <zegenie@gmail.com>
2018-08-28 13:58:05 +0200

    remove outdated firehouse theme

--- themes/firehouse/README.md
+++
@@ -1,2 +0,0 @@
-"Firehouse" is a dark, vibrant, red variant of the default theme for The Bug Genie.
-***Not compatible with version 4.x***
--- themes/firehouse/theme.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    /**
-     * Configuration for theme
-     */
-
-    $tbg_response->addStylesheet('assets/css/theme.css');
-    \thebuggenie\core\framework\Settings::setIconsetName('oxygen');
