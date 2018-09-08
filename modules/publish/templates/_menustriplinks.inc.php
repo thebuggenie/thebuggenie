338fcb2fad7e4b47e60ecd703e1aa387af61f3e9
Daniel André Eikeland <zegenie@gmail.com>
2018-09-08 13:55:13 +0200

    fix incorrect wiki links

--- modules/publish/templates/_menustriplinks.inc.php
+++ modules/publish/templates/_menustriplinks.inc.php
@@ -1,25 +1,25 @@
 <li class="with-dropdown <?php if (strpos($selected_tab, 'publish_') === 0): ?>selected<?php endif; ?>">
     <?php $dropper_class = (count(\thebuggenie\core\entities\Project::getAllRootProjects())) ? 'dropper' : ''; ?>
     <?php if (!isset($wiki_url)): ?>
-        <?= link_tag(((isset($project_url)) ? $project_url : $url), fa_image_tag('newspaper-o', array(), false, 'publish') . \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle($project instanceof \thebuggenie\core\entities\Project) . fa_image_tag('caret-down', ['class' => 'dropdown-indicator']), ['class' => $dropper_class]); ?>
+        <?= link_tag(((isset($project_url)) ? $project_url : $url), fa_image_tag('newspaper-o', [], false, 'publish') . \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle($project instanceof \thebuggenie\core\entities\Project) . fa_image_tag('caret-down', ['class' => 'dropdown-indicator']), ['class' => $dropper_class]); ?>
     <?php else: ?>
         <?= link_tag($wiki_url, \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle($project instanceof \thebuggenie\core\entities\Project) . fa_image_tag('caret-down', ['class' => 'dropdown-indicator']), ['target' => 'blank', 'class' => $dropper_class]) ?>
     <?php endif; ?>
     <div id="wiki_dropdown_menu" class="tab_menu_dropdown popup_box two-columns wide-right">
         <ul>
             <li class="header"><?= __('Quick links'); ?></li>
-            <li><?php echo link_tag($wiki_url, \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle(false), ['target' => 'blank']) ?></li>
+            <li><?php echo link_tag(make_url('publish_article', ['article_name' => 'MainPage']), \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle(false)) ?></li>
             <?php if ($project instanceof \thebuggenie\core\entities\Project): ?>
-                <li><?php echo link_tag($wiki_url, \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle($project instanceof \thebuggenie\core\entities\Project), array('target' => 'blank')) ?></li>
+                <li><?php echo link_tag(make_url('publish_article', ['article_name' => $project->getKey().':MainPage']), \thebuggenie\core\framework\Context::getModule('publish')->getMenuTitle($project instanceof \thebuggenie\core\entities\Project)) ?></li>
             <?php endif; ?>
             <?php if (count(\thebuggenie\core\entities\Project::getAllRootProjects(false)) > (int) ($project instanceof \thebuggenie\core\entities\Project)): ?>
                 <li class="header"><?= __('Project wikis'); ?></li>
                 <?php foreach (\thebuggenie\core\entities\Project::getAllRootProjects(false) as $root_project): ?>
                     <?php if (!$root_project->hasAccess() || $root_project->isArchived() || (isset($project_url) && $root_project->getID() == $project->getID())) continue; ?>
                     <?php if (!$root_project->hasWikiURL()): ?>
-                        <li><?= link_tag(make_url('publish_article', array('article_name' => $root_project->getName().':MainPage')), image_tag($root_project->getSmallIconName(), array('class' => 'icon'), $root_project->hasSmallIcon()) . $root_project->getName()); ?></li>
+                        <li><?= link_tag(make_url('publish_article', ['article_name' => $root_project->getKey().':MainPage']), image_tag($root_project->getSmallIconName(), ['class' => 'icon'], $root_project->hasSmallIcon()) . $root_project->getName()); ?></li>
                     <?php else: ?>
-                        <li><?= link_tag($root_project->getWikiURL(), image_tag($root_project->getSmallIconName(), array('class' => 'icon'), $root_project->hasSmallIcon()) . $root_project->getName(), array('target' => 'blank')) ?></li>
+                        <li><?= link_tag($root_project->getWikiURL(), image_tag($root_project->getSmallIconName(), ['class' => 'icon'], $root_project->hasSmallIcon()) . $root_project->getName(), ['target' => 'blank']) ?></li>
                     <?php endif; ?>
                 <?php endforeach; ?>
             <?php endif; ?>
@@ -36,7 +36,7 @@
             <?php elseif (!isset($wiki_url)): ?>
                 <li class="header"><?= __('Project content'); ?></li>
                 <li class="form-container">
-                    <form action="<?php echo make_url('publish_find_project_articles', array('project_key' => $project->getName())); ?>" method="get" accept-charset="<?php echo \thebuggenie\core\framework\Context::getI18n()->getCharset(); ?>">
+                    <form action="<?php echo make_url('publish_find_project_articles', ['project_key' => $project->getName()]); ?>" method="get" accept-charset="<?php echo \thebuggenie\core\framework\Context::getI18n()->getCharset(); ?>">
                         <input type="search" name="articlename" placeholder="<?php echo $quicksearch_title; ?>">
                     </form>
                 </li>
