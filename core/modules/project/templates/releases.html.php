1a056635c88687e55119752a628ee902345b9928
Daniel André Eikeland <zegenie@gmail.com>
2018-09-11 10:35:29 +0200

    add link to release center from release list

--- core/modules/project/templates/releases.html.php
+++ core/modules/project/templates/releases.html.php
@@ -8,17 +8,22 @@
 <div id="project_releases" class="project_info_container">
     <div class="project_left_container">
         <div class="project_left">
-            <h3><?php echo __('Release selection'); ?></h3>
+            <h3><?= __('Release selection'); ?></h3>
             <ul class="simple_list">
-                <li class="selected"><a href="javascript:void(0);" onclick="$$('.releases_list').each(function (r) { (r.hasClassName('active_releases')) ? r.show() : r.hide() }); TBG.Project.toggleLeftSelection(this);"><?php echo __('All active releases'); ?></a></li>
-                <li ><a href="javascript:void(0);" onclick="$$('.releases_list').each(function (r) { (r.hasClassName('archived_releases')) ? r.show() : r.hide() }); TBG.Project.toggleLeftSelection(this);"><?php echo __('Archived releases'); ?></a></li>
+                <li class="selected"><a href="javascript:void(0);" onclick="$$('.releases_list').each(function (r) { (r.hasClassName('active_releases')) ? r.show() : r.hide() }); TBG.Project.toggleLeftSelection(this);"><?= __('All active releases'); ?></a></li>
+                <li ><a href="javascript:void(0);" onclick="$$('.releases_list').each(function (r) { (r.hasClassName('archived_releases')) ? r.show() : r.hide() }); TBG.Project.toggleLeftSelection(this);"><?= __('Archived releases'); ?></a></li>
             </ul>
         </div>
     </div>
     <div class="project_right_container">
         <div class="project_right" id="project_releases_container">
+            <?php if ($tbg_user->canEditProjectDetails($selected_project)): ?>
+                <div class="project_save_container">
+                    <?= link_tag(make_url('project_release_center', array('project_key' => $selected_project->getKey())), __('Manage project releases'), ['class' => 'button button-silver']); ?>
+                </div>
+            <?php endif; ?>
             <div class="active_releases releases_list">
-                <h3><?php echo __('Active project releases'); ?></h3>
+                <h3><?= __('Active project releases'); ?></h3>
                 <?php if (count($active_builds[0])): ?>
                     <ul class="simple_list">
                     <?php foreach ($active_builds[0] as $build): ?>
@@ -26,11 +31,11 @@
                     <?php endforeach; ?>
                     </ul>
                 <?php else: ?>
-                    <div class="faded_out"><?php echo __('There are no active releases for this project'); ?></div>
+                    <div class="faded_out"><?= __('There are no active releases for this project'); ?></div>
                 <?php endif; ?>
                 <?php if ($selected_project->isEditionsEnabled()): ?>
                     <?php foreach ($selected_project->getEditions() as $edition_id => $edition): ?>
-                        <h4><?php echo __('Active %edition_name releases', array('%edition_name' => $edition->getName())); ?></h4>
+                        <h4><?= __('Active %edition_name releases', array('%edition_name' => $edition->getName())); ?></h4>
                         <?php if (count($active_builds[$edition_id])): ?>
                             <ul class="simple_list">
                             <?php foreach ($active_builds[$edition_id] as $build): ?>
@@ -38,13 +43,13 @@
                             <?php endforeach; ?>
                             </ul>
                         <?php else: ?>
-                            <div class="faded_out"><?php echo __('There are no active releases for this edition'); ?></div>
+                            <div class="faded_out"><?= __('There are no active releases for this edition'); ?></div>
                         <?php endif; ?>
                     <?php endforeach; ?>
                 <?php endif; ?>
             </div>
             <div class="archived_releases releases_list" style="display: none;">
-                <h3><?php echo __('Archived project releases'); ?></h3>
+                <h3><?= __('Archived project releases'); ?></h3>
                 <?php if (count($archived_builds[0])): ?>
                     <ul class="simple_list">
                     <?php foreach ($archived_builds[0] as $build): ?>
@@ -52,11 +57,11 @@
                     <?php endforeach; ?>
                     </ul>
                 <?php else: ?>
-                    <div class="faded_out"><?php echo __('There are no archived releases for this project'); ?></div>
+                    <div class="faded_out"><?= __('There are no archived releases for this project'); ?></div>
                 <?php endif; ?>
                 <?php if ($selected_project->isEditionsEnabled()): ?>
                     <?php foreach ($selected_project->getEditions() as $edition_id => $edition): ?>
-                        <h4><?php echo __('Archived %edition_name releases', array('%edition_name' => $edition->getName())); ?></h4>
+                        <h4><?= __('Archived %edition_name releases', array('%edition_name' => $edition->getName())); ?></h4>
                         <?php if (count($archived_builds[$edition_id])): ?>
                             <ul class="simple_list">
                             <?php foreach ($archived_builds[$edition_id] as $build): ?>
@@ -64,7 +69,7 @@
                             <?php endforeach; ?>
                             </ul>
                         <?php else: ?>
-                            <div class="faded_out"><?php echo __('There are no archived releases for this edition'); ?></div>
+                            <div class="faded_out"><?= __('There are no archived releases for this edition'); ?></div>
                         <?php endif; ?>
                     <?php endforeach; ?>
                 <?php endif; ?>
